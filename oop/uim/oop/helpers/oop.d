f20b3b5ce0156934c88b56ab32c745e117b8f227
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-18 07:11:27 +0100

    Updates

---
+++ oop/uim/oop/helpers/oop.d
@@ -0,0 +1,14 @@
+module uim.oop.helpers.oop;
+
+bool isSubclassOf(C, B)(C aClass, B aBaseClass) {
+    return (cast(B)aClass ? true : false);
+}
+
+bool isNull(Object instance) {
+    return (instance is null);
+}
+/* Found in web:
+bool isNull(T)(T value) if (is(T == class) || isPointer!T) {
+	return value is null;
+}
+*/
--- oop/uim/oop/helpers/package.d
+++ oop/uim/oop/helpers/package.d
@@ -1,5 +1,5 @@
 module uim.oop.helpers;
 
-bool isSubclassOf(C, B)(C aClass, B aBaseClass) {
-    return (cast(B)aClass ? true : false);
+public {
+    import uim.oop.helpers.oop;
 }

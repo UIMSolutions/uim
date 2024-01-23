f71347a2546ee27f8c187d2716ca5a790ca49078
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 07:56:15 +0100

    Adding filesystems

---
+++ filesystems/uim/filesystems/tests/package.d
@@ -0,0 +1,14 @@
+/***********************************************************************************
+*	Copyright: ©2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.filesystems.tests;
+
+public {
+	import uim.filesystems.tests.file;
+	import uim.filesystems.tests.filesystem;
+	import uim.filesystems.tests.folder;
+	import uim.filesystems.tests.general;
+	import uim.filesystems.tests.link;
+}

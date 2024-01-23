53fb639653b27bae710b5058ed2394b744f4748e
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 07:56:19 +0100

    Adding filesystems

---
+++ oop/uim copy/filesystems/classes/cache.d
@@ -0,0 +1,24 @@
+/***********************************************************************************
+*	Copyright: ©2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.filesystems.classes.cache;
+
+import uim.filesystems;
+
+unittest { 
+  version(testUimFilesystems) { 
+    debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); 
+  }
+}
+
+@safe:
+class DCacheFilesystem : DFilesystem {
+  this() { super(); }
+}
+auto CacheFilesystem() { return new DCacheFilesystem; }
+
+unittest {
+	// TODO add tests
+}

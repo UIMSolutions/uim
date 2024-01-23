15dc5f3d9f61db954bf8291c7230a1ccb95f2a8b
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 08:12:50 +0100

    Copyright

--- core/uim/core/datatypes/package.d
+++ core/uim/core/datatypes/package.d
@@ -1,7 +1,7 @@
 ﻿/***********************************************************************************************
-*	Copyright: © 2017-2021 UI Manufaktur UG
+*	Copyright: © 2017- 2024 Ozan Nurettin Süel (sicherheitsschmiede)
 *	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
-*	Authors: Before 2022 UI Manufaktur UG Team / Since 2022 - Ozan Nurettin Süel (sicherheitsschmiede) 
+*	Authors: Before 2022 Ozan Nurettin Süel (sicherheitsschmiede) Team / Since 2022 - Ozan Nurettin Süel (sicherheitsschmiede) 
 *	Documentation [DE]: https://www.sicherheitsschmiede.de/docus/uim-core/datatypes/overview
 ************************************************************************************************/
 module uim.core.datatypes;

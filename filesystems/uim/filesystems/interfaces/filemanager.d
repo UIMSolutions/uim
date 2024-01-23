da6cc5004893e9d45c494f9cb075b5f7eb0cf424
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 08:13:04 +0100

    Copyright

--- filesystems/uim/filesystems/interfaces/filemanager.d
+++ filesystems/uim/filesystems/interfaces/filemanager.d
@@ -1,7 +1,7 @@
 /***********************************************************************************************************************
-*	Copyright: © 2017-2022 UI Manufaktur UG / 2022 Ozan Nurettin Süel (sicherheitsschmiede)                              *
+*	Copyright: © 2017- 2024 Ozan Nurettin Süel (sicherheitsschmiede) / 2022 Ozan Nurettin Süel (sicherheitsschmiede)                              *
 *	License: Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt]                                       *
-*	Authors: UI Manufaktur UG Team, Ozan Nurettin Süel (Sicherheitsschmiede)										                         * 
+*	Authors: Ozan Nurettin Süel (sicherheitsschmiede) Team, Ozan Nurettin Süel (Sicherheitsschmiede)										                         * 
 ***********************************************************************************************************************/
 module uim.filesystems.interfaces.filemanager;
 

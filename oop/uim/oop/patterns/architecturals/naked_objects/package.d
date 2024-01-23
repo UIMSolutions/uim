fe0cc2c61d9162950ff6641fb61eb981173212df
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 08:14:45 +0100

    Copyright

--- oop/uim/oop/patterns/architecturals/naked_objects/package.d
+++ oop/uim/oop/patterns/architecturals/naked_objects/package.d
@@ -16,7 +16,7 @@ foreword by Trygve Reenskaug, who first formulated the model–view–controller
 model-view-controller (MVC) than many of the subsequent interpretations and implementations.
 **/
 /***********************************************************************************
-*	Copyright: ©2015 -2024 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	Copyright: ©2015 - 2024 Ozan Nurettin Süel (sicherheitsschmiede)                   *
 *	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
 *	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
 ***********************************************************************************/

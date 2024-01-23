4dda351ab0272af1e3fcb2ba35854a642b80389c
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 07:56:45 +0100

    Adding filesystems

---
+++ oop/uim copy/filesystems/mixins/entrymanager.d
@@ -0,0 +1,16 @@
+/***********************************************************************************
+*	Copyright: ©2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.filesystems.mixins.entrymanager;
+
+import uim.filesystems;
+
+@safe:
+template EntryManagerTemplate() {
+  bool isHidden(string aPath) {
+    return this.isHidden(aPath.split(pathSeparator));    
+  }
+}
+

e0f7be0f4ffcf3a6d80351a8be6467fc5b5c64a3
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 07:59:34 +0100

    Corrections

--- oop/uim copy/filesystems/classes/virtual/package.d
+++
@@ -1,8 +0,0 @@
-module uim.filesystems.classes.virtual;
-
-public {
-  import uim.filesystems.classes.virtual.file;
-  import uim.filesystems.classes.virtual.filesystem;
-  import uim.filesystems.classes.virtual.folder;
-  import uim.filesystems.classes.virtual.link;
-}

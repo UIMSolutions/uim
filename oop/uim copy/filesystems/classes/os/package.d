a8b412a91cc51be9edcfbee8772d34b70c219c90
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 07:56:28 +0100

    Adding filesystems

---
+++ oop/uim copy/filesystems/classes/os/package.d
@@ -0,0 +1,11 @@
+/***********************************************************************************
+*	Copyright: ©2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.filesystems.classes.os;
+
+public {
+  import uim.filesystems.classes.os.linux;
+  import uim.filesystems.classes.os.windows;
+}
---
+++ oop/uim copy/filesystems/classes/os/windows/filesystem.d
@@ -0,0 +1,26 @@
+/***********************************************************************************
+*	Copyright: ©2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.filesystems.classes.os.windows.filesystem;
+
+import uim.filesystems;
+
+unittest { 
+  version(testUimFilesystems) { 
+    debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); 
+  }
+}
+
+@safe:
+class DWindowsFilesystem : DFilesystem {
+  mixin(FilesystemThis!("WindowsFilesystem"));
+
+  override void initialize(Json configSettings = Json(null)) {
+    super.initialize(configSettings);
+
+		pathSeparator("\\");
+  }
+}
+mixin(FilesystemCalls!("WindowsFilesystem"));

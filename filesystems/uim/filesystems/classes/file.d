8250c7be4f65a4224e9ca9ab8fec064cfacfe9cd
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 07:55:35 +0100

    Adding filesystems

---
+++ filesystems/uim/filesystems/classes/file.d
@@ -0,0 +1,174 @@
+/***********************************************************************************
+*	Copyright: ©2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.filesystems.classes.file;
+
+import uim.filesystems;
+
+unittest { 
+  version(testUimFilesystems) { 
+    debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); 
+  }
+}
+
+@safe:
+class DFile : DFilesystemEntry, IFile {
+	mixin(FileThis!("File"));
+
+  override void initialize(Json configSettings = Json(null)) { // Hook
+    super.initialize(configSettings);
+  }
+
+	// #region inherited properties
+	
+
+	// #region Properties
+		// Returns the folder object for the parent of the specified file.
+		override bool isFile() {
+			return exists;
+		}	
+
+		// Check if file exits
+		override bool exists() {
+			if (parentFolder) return parentFolder.existsFolder(path, name); 
+			return (hasFilesystem ? filesystem.existsFolder(path, name) : false);
+		}
+
+		// Sets or returns the attributes of a specified file.
+		size_t attributes() {
+			return 0;
+		} 	
+
+		void attributes(size_t newAttributes) {
+		} 	
+
+		// Returns the date and time when a specified file was created.
+		long createdOn() {
+			return 0;
+		} 	
+		
+		// Returns the date and time when a specified file was last accessed.
+		long accessedOn() {
+			return 0;
+		} 	
+		
+		// Returns the date and time when a specified file was last modified.
+		long modifiedOn() {
+	    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+			return 0;			
+		} 	
+		
+		// Returns the drive letter of the drive where a specified file or folder resides.
+		string driveName() {
+	    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+			return null;
+		} 	
+		
+		// Returns the type of a specified file.
+		string type() {
+	    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+			return null;
+		} 	
+	// #endregion Properties
+
+	void[] readData(size_t dataSitze = size_t.max) { 
+    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+		return (hasFilesystem ? filesystem.readFromFile(path, name, dataSitze) : null);
+	}
+	
+	void writeData(const void[] data) {
+    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+		
+		if (hasFilesystem) filesystem.writeToFile(path, name, data);
+	}
+
+	void appendData(const void[] data) {
+    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+		if (hasFilesystem) filesystem.appendToFile(path, name, data);		
+	}
+
+	string readText() { 
+    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+		return (hasFilesystem ? filesystem.readTextFromFile(path, name) : null);
+	}
+	
+	void writeText(string aText) {
+    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+		if (hasFilesystem) filesystem.writeTextToFile(path, name, aText);
+	}
+
+	void appendText(string aText) {
+    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+		if (hasFilesystem) filesystem.appendTextToFile(path, name, aText);
+	}
+
+	string[] readLines() { 
+    version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+		return (hasFilesystem ? filesystem.readLines(path, name) : null);
+	}
+
+	Json readJson() {
+		version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+		return Json(null);
+	}
+	
+	bool writeJson(Json jsonData) {
+		version(testUimFilesystems) { debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); }
+
+		return false;
+	}
+
+	// #region Methods
+		// Copies a specified file from one location to another.
+		bool copy(string toPath) {
+			return false;
+		}
+
+		bool copy(string[] toPath) {
+			return false;
+		} 	
+
+		bool copy(IFolder toFolder) {
+			return false;
+		} 	
+		
+		// Moves file from one location to another.
+		bool move(string toPath) {
+			return false;
+		}
+
+		bool move(string[] toPath) {
+			return false;
+		} 	
+		
+		bool move(IFolder toFolder) {
+			return false;
+		} 	
+
+		// Deletes file.
+		bool remove() {
+			return (hasFilesystem ? filesystem.removeFile(path, name) : false);
+		}
+
+		override string toString() {
+			return className~": "~name;
+		}
+		// TODO: OpenAsTextStream 	Opens a specified file and returns a TextStream object to access the file.
+	// #endregion Methods
+}
+
+unittest {
+	// TODO Adding Tests
+}
---
+++ filesystems/uim/filesystems/classes/folder.d
@@ -0,0 +1,112 @@
+/***********************************************************************************
+*	Copyright: ©2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.filesystems.classes.folder;
+
+import uim.filesystems;
+
+unittest { 
+  version(testUimFilesystems) { 
+    debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); 
+  }
+}
+
+@safe:
+class DFolder : DFilesystemEntry, IFolder, IFolderManager, IFileManager, ILinkManager {
+  mixin(FolderThis!("Folder"));
+
+  override void initialize(Json configSettings = Json(null)) { // Hook
+    super.initialize(configSettings);
+  }
+
+  protected string _pathSeparator;
+  void pathSeparator(string newSeparator) {
+    _pathSeparator = newSeparator;
+  }
+  string pathSeparator() {
+    return (hasFilesystem ? filesystem.pathSeparator : null);
+  }
+
+	override bool exists() {
+		return (hasFilesystem ? filesystem.existsFolder(path, name) : false);
+	}
+
+	override bool isFolder() {
+		return exists;
+	}
+
+	// Sets or returns the attributes of a specified folder.
+  long attributes() {
+		// TODO
+		return 0;		
+	}
+  void attributes(long newAttributes) {
+		// TODO
+	}
+
+  // Returns the date and time when a specified folder was created.
+  long createdOn() {
+		// TODO
+		return 0;		
+	}
+
+  // Returns the date and time when a specified folder was last accessed.
+  long accessedOn() {
+		// TODO
+		return 0;		
+	}
+  // Returns the date and time when a specified folder was last modified.
+  long modifiedOn() {
+		// TODO
+		return 0;		
+	}
+  // Returns the drive letter of the drive where the specified folder resides.
+  IDrive drive() {
+		// TODO
+		return null;		
+	}
+
+  bool hasEntries() {
+		// TODO
+		return false;		
+	}
+
+  bool isEmpty() {
+		// TODO
+		return true;		
+	}
+
+  // Returns True if a folder is the root folder and False if not.
+  bool isRootFolder() {
+		// TODO
+		return false;		
+	}
+
+  // Returns the type of a specified folder. */
+  string type() {
+		// TODO
+		return null;		
+	} 
+
+	// #region Methods
+		// Deletes file.
+		bool remove() {
+			return false;
+		}
+	// #endregion Methods
+
+  mixin FolderManagerTemplate!();    
+  mixin FileManagerTemplate!();
+  mixin LinkManagerTemplate!();
+
+  override string toString() {
+    return className~": "~name;
+  }
+}
+mixin(FolderCalls!("Folder"));
+
+unittest {
+	assert(Folder(), "Could not create file object");
+}

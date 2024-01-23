551c7049f5f144001ca1baa952f6d37a2f92305d
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-23 07:56:23 +0100

    Adding filesystems

---
+++ oop/uim copy/filesystems/classes/memory/folder.d
@@ -0,0 +1,20 @@
+/***********************************************************************************
+*	Copyright: ©2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.filesystems.classes.memory.folder;
+
+import uim.filesystems;
+
+unittest { 
+  version(testUimFilesystems) { 
+    debug writeln("\n", __MODULE__~":"~__PRETTY_FUNCTION__); 
+  }
+}
+
+@safe:
+class DMemoryFolder : DFolder {
+  mixin(FolderThis!("MemoryFolder"));
+}
+mixin(FolderCalls!("MemoryFolder"));

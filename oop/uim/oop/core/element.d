bdd1523c462c1ff2004adb806368872061892a7a
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-25 13:21:35 +0100

    replace with property with new className function

--- oop/uim/oop/core/element.d
+++ oop/uim/oop/core/element.d
@@ -10,62 +10,62 @@ import uim.oop;
 @safe:
 
 class DOOPElement {
-	this() {
-	}
+  this() {
+  }
 
-	this(string newName) {
-		this();
-		this.name = newName;
-	}
+  this(string newName) {
+    this();
+    this.name = newName;
+  }
 
-	string namespace() {
-		return "uim.oop";
-	}
+  string namespace() {
+    return "uim.oop";
+  }
 
-	string fullname() {
-		return namespace ~ "." ~ this.className;
-	}
+  string fullname() {
+    return namespace ~ "." ~ this.className;
+  }
 
-	string fullpath() {
-		return fullname.replace(".", "/");
-	}
+  string fullpath() {
+    return fullname.replace(".", "/");
+  }
 
-	mixin(PropertyDefinition!("string", "_name", "name", true, true));
+  mixin(PropertyDefinition!("string", "_name", "name", true, true));
 
-	/* 	Bson toBson() {
+  /* 	Bson toBson() {
 		Bson result = Bson.emptyObject;
 
 		return result;
 	}*/
-	Json toJson() {
-		auto result = Json.emptyObject;
-		result["namespace"] = namespace;
-		result["classname"] = this.className;
-		result["fullname"] = fullname;
-		result["namespace"] = fullpath;
-		return result;
-	}
+  Json toJson() {
+    auto result = Json.emptyObject;
+    result["namespace"] = namespace;
+    result["classname"] = this.className;
+    result["fullname"] = fullname;
+    result["namespace"] = fullpath;
+    return result;
+  }
 
-	override string toString() {
-		return `{"name":"%s"}`.format(_name);
-	}
+  override string toString() {
+    return `{"name":"%s"}`.format(_name);
+  }
 }
 
 auto OOPElement() {
-	return new DOOPElement;
+  return new DOOPElement;
 }
 
 auto OOPElement(string aName) {
-	return new DOOPElement(aName);
+  return new DOOPElement(aName);
 }
 
 unittest {
-	auto element = new DOOPElement;
-	assert(element.namespace == "uim.oop");
-	assert(element.classname == "Element");
-	assert(element.fullname == "uim.oop.Element");
-	assert(element.fullpath == "uim/oop/Element");
+  auto element = new DOOPElement;
+  assert(element.namespace == "uim.oop");
+  assert(element.className == "Element");
+  assert(element.fullname == "uim.oop.Element");
+  assert(element.fullpath == "uim/oop/Element");
 
-	assert(OOPElement("test").name == "test");
-	assert(OOPElement("test").name("test2").name == "test2");
+  assert(OOPElement("test").name == "test");
+  assert(OOPElement("test").name("test2").name == "test2");
 }

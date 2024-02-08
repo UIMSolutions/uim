bbd2ce88bc7c4f0e4820f12140ad72127f24a54b
UIMSolutions <ui.manufaktur@gmail.com>
2024-02-08 13:39:04 +0100

    Correct Copyright years

--- oop/uim/oop/patterns/architecturals/services_locators/package.d
+++ oop/uim/oop/patterns/architecturals/services_locators/package.d
@@ -7,7 +7,7 @@ are cleanly listed at the beginning of the whole application design, consequentl
 connecting objects. Critics of the pattern argue that it is an anti-pattern which obscures dependencies and makes software harder to test.
 **/
 /***********************************************************************************
-*	Copyright: ©2015 - 2024 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	Copyright: ©2015-2024 Ozan Nurettin Süel (sicherheitsschmiede)                   *
 *	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
 *	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
 ***********************************************************************************/

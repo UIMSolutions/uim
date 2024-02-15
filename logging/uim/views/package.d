0e0934dd951abfc9e35d2b2cf7c76d94edecb652
UIMSolutions <ui.manufaktur@gmail.com>
2024-02-15 09:45:28 +0100

    Updates

--- logging/uim/views/package.d
+++ logging/uim/views/package.d
@@ -3,7 +3,7 @@
 *	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
 *	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
 ***********************************************************************************/
-module uim.views;
+module uim.logging;
 
 mixin(ImportPhobos!());
 
@@ -21,10 +21,10 @@ public { // uim libraries
 }
 
 public { // uim.filesystem libraries
-  import uim.views.classes;
-  import uim.views.exceptions;
-  import uim.views.helpers;
-  import uim.views.interfaces;
-  import uim.views.mixins;
-  import uim.views.tests;
+  import uim.logging.classes;
+  import uim.logging.exceptions;
+  import uim.logging.helpers;
+  import uim.logging.interfaces;
+  import uim.logging.mixins;
+  import uim.logging.tests;
 }

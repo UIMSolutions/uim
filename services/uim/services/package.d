241abf6d3eed1eb05878a9f233b6274fbdd6f0ed
UIMSolutions <ui.manufaktur@gmail.com>
2024-02-15 11:29:29 +0100

    Updates

---
+++ services/uim/services/package.d
@@ -0,0 +1,30 @@
+/***********************************************************************************
+*	Copyright: ©2015-2024 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
+*	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
+***********************************************************************************/
+module uim.services;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+	import colored;
+	import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+  import uim.models;
+}
+
+public { // uim.filesystem libraries
+  import uim.services.classes;
+  import uim.services.exceptions;
+  import uim.services.helpers;
+  import uim.services.interfaces;
+  import uim.services.mixins;
+  import uim.services.tests;
+}

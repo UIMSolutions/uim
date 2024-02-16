e00bb2c7cbb629f0f19e29253736d51a8ef91a0c
UIMSolutions <ui.manufaktur@gmail.com>
2024-02-16 15:52:33 +0100

    Updates

--- databases/uim/databases/package.d
+++ databases/uim/databases/package.d
@@ -3,7 +3,7 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.forms;
+module uim.databases;
 
 mixin(ImportPhobos!());
 
@@ -19,11 +19,11 @@ public { // Required uim libraries
   import uim.models;
 }
 
-public { // uim.forms packages
-  import uim.forms.classes;
-  import uim.forms.exceptions;
-  import uim.forms.interfaces;
-  import uim.forms.helpers;
-  import uim.forms.mixins;
-  import uim.forms.tests; 
+public { // uim.databases packages
+  import uim.databases.classes;
+  import uim.databases.exceptions;
+  import uim.databases.interfaces;
+  import uim.databases.helpers;
+  import uim.databases.mixins;
+  import uim.databases.tests; 
 }

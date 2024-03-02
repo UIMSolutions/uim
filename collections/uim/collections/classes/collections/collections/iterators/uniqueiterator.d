38eeec36dfabf05c9620b8d0928773523bcd8db2
UIMSolutions <ui.manufaktur@gmail.com>
2024-03-02 09:41:49 +0100

    Updates

---
+++ collections/uim/collections/classes/collections/collections/iterators/uniqueiterator.d
@@ -0,0 +1,39 @@
+module uim.cake.collections.iterators;
+
+import uim.cake;
+
+@safe:
+
+/**
+ * Creates a filtered iterator from another iterator. The filtering is done by
+ * passing a callback auto to each of the elements and taking them out if
+ * the value returned is not unique.
+ */
+class UniqueIterator : Collection {
+    /**
+     * Creates a filtered iterator using the callback to determine which items are
+     * accepted or rejected.
+     *
+     * The callback is passed the value as the first argument and the key as the
+     * second argument.
+     * Params:
+     * iterable someItems The items to be filtered.
+     * @param callable aCallback Callback.
+     */
+    this(iterable someItems, callable aCallback) {
+        if (!cast(Iterator)someItems) {
+            someItems = new Collection(someItems);
+        }
+        auto myunique = [];
+        auto myuniqueValues = [];
+        someItems.byKeyValue
+            .each!((kv) {
+                auto compareValue = aCallback(kv.value, kv.key);
+                if (!in_array(compareValue, uniqueValues, true)) {
+                    unique[kv.key] = kv.value;
+                    uniqueValues ~= compareValue;
+                }
+            }); 
+        super($unique);
+    }
+}

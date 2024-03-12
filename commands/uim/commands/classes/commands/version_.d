10df3b9b358402c5d54f81f9aeed8f52d9dd3ec0
UIMSolutions <ui.manufaktur@gmail.com>
2024-03-12 17:55:09 +0100

    Updates

---
+++ commands/uim/commands/classes/commands/version_.d
@@ -0,0 +1,26 @@
+module commands.uim.commands.classes.commands.version_;
+
+import uim.commands;
+
+@safe:
+
+// Print out the version of UIM in use.
+class DVersionCommand : DCommand {
+  mixin(CommandThis!("Version"));
+
+  override bool initialize(IData[string] initData = null) {
+    if (!super.initialize(initData)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  int execute(IData[string] arguments, ConsoleIo aConsoleIo) {
+    aConsoleIo.writeln(Configure.currentVersion());
+
+    return CODE_SUCCESS;
+  }
+}
+
+mixin(CommandCalls!("Version"));

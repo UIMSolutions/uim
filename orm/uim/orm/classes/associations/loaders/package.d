0d47d5c5e1794a9a91e51cdf2ee3168b98b47670
UIMSolutions <ui.manufaktur@gmail.com>
2024-03-14 08:06:13 +0100

    Bugfixing

--- orm/uim/orm/classes/associations/loaders/package.d
+++
@@ -1,5 +0,0 @@
-module uim.orm.classes.associations.loaders;
-
-public {
-    import uim.orm.classes.associations.loaders.select;
-}
--- orm/uim/orm/classes/associations/package.d
+++
@@ -1,8 +0,0 @@
-module uim.orm.classes.associations;
-
-public { // Packages
-    import uim.orm.classes.behaviors;
-    import uim.orm.classes.locators;
-    import uim.orm.classes.queries;
-    import uim.orm.classes.rules;
-}

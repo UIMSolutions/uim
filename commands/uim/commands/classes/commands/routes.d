c24cefcf555c5880a66ec94fa440c8759f714d10
UIMSolutions <ui.manufaktur@gmail.com>
2024-03-15 12:49:50 +0100

    Updates

---
+++ commands/uim/commands/classes/commands/routes.d
@@ -0,0 +1,108 @@
+module uim.commands;
+
+import uim.commands;
+
+@safe:
+
+// Provides interactive CLI tools for routing.
+class RoutesCommand : DCommand {
+   mixin(CommandThis!("RoutesCommand"));
+
+  	override bool initialize(IData[string] initData = null) {
+		if (!super.initialize(initData)) { return false; }
+		
+		return true;
+	}
+
+    // Display all routes in an application
+  int execute(IData[string] arguments, IConsoleIo aConsoleIo) {
+        auto myheader = ["Route name", "URI template", "Plugin", "Prefix", "Controller", "Action", "Method(s)"];
+        if (commandArguments.getOption("verbose")) {
+             aHeader ~= "Defaults";
+        }
+        auto myavailableRoutes = Router.routes();
+        output =  someDuplicateRoutesCounter = [];
+
+        someAvailableRoutes.each!((route) {
+             someMethods = isSet(route.defaults["_method"]) ? (array)route.defaults["_method"] : [""];
+
+             anItem = [
+                route.options.get("_name", route.name),
+                route.template,
+                route.defaults.get("plugin", ""),
+                route.defaults.get("prefix", ""),
+                route.defaults.get("controller", ""),
+                route.defaults.get("action", ""),
+                someMethods.join(", "),
+            ];
+
+            if (commandArguments.getOption("verbose")) {
+                ksort(route.defaults);
+                 anItem ~= json_encode(route.defaults, JSON_THROW_ON_ERROR);
+            }
+            output ~=  anItem;
+
+            someMethods.each!((method) {
+                if (!isSet(someDuplicateRoutesCounter[route.template][myMethod])) {
+                     someDuplicateRoutesCounter[route.template][method] = 0;
+                }
+                someDuplicateRoutesCounter[route.template][method]++;
+            });
+        });
+
+        if (commandArguments.getOption("sort")) {
+            usort( output, auto (a, b) {
+                return strcasecmp(a[0], b[0]);
+            });
+        }
+        array_unshift( output,  aHeader);
+
+         aConsoleIo.helper("table").output( output);
+         aConsoleIo.writeln();
+
+         someDuplicateRoutes = [];
+
+        foreach (myRoute;  someAvailableRoutes) {
+            string[] someMethods = isSet(myRoute.defaults["_method"]) ? (array)myRoute.defaults["_method"] : [""];
+
+            foreach (myMethod; someMethods) {
+                if (
+                     someDuplicateRoutesCounter[myRoute.template][myMethod] > 1 ||
+                    (myMethod.isEmpty && count(someDuplicateRoutesCounter[myRoute.template]) > 1) ||
+                    (myMethod != "" && isSet(someDuplicateRoutesCounter[myRoute.template][""]))
+                ) {
+                     someDuplicateRoutes ~= [
+                        myRoute.options["_name"] ?? myRoute.name,
+                        myRoute.template,
+                        myRoute.defaults.get("plugin", ""),
+                        myRoute.defaults.get("prefix", ""),
+                        myRoute.defaults.get("controller", ""),
+                        myRoute.defaults.get("action", ""),
+                        someMethods.join(", "),
+                    ];
+
+                    break;
+                }
+            }
+        }
+        if (someDuplicateRoutes) {
+            array_unshift(someDuplicateRoutes,  aHeader);
+            aConsoleIo.warning("The following possible route collisions were detected.");
+            aConsoleIo.helper("table").output(someDuplicateRoutes);
+            aConsoleIo.writeln();
+        }
+        return CODE_SUCCESS;
+    }
+
+    ConsoleOptionParser buildOptionParser(ConsoleOptionParser  aParser) {
+         aParser
+            .description("Get the list of routes connected in this application.")
+            .addOption("sort", [
+                "help": "sorts alphabetically by route name A-Z",
+                "short": "s",
+                "boolean": true,
+            ]);
+
+        return aParser;
+    }
+}

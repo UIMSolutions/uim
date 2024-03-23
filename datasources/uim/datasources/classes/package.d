9379af23badfe4834ecc3a79a32fb060b3643327
UIMSolutions <ui.manufaktur@gmail.com>
2024-03-23 14:06:53 +0100

    Updates

--- datasources/uim/datasources/classes/package.d
+++ datasources/uim/datasources/classes/package.d
@@ -3,4 +3,9 @@ module uim.datasources.classes;
 public {
     import uim.datasources.classes.connections;
     import uim.datasources.classes.entities;
+    import uim.datasources.classes.locators;
+    import uim.datasources.classes.pagings;
+    import uim.datasources.classes.repositories;
+    import uim.datasources.classes.resultsets;
+    import uim.datasources.classes.rules;
 }

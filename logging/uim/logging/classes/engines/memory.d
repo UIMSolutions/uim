671ba068cbf535e5894d839bd7da56c963e0898f
Business in D <ui.manufaktur@gmail.com>
2024-04-02 20:10:25 +0200

    Update and rename array.dx to memory.d

--- logging/uim/logging/classes/engines/memory.d
+++ logging/uim/logging/classes/engines/memory.d
@@ -1,17 +1,17 @@
-module uim.logging.classes.engines.array;
+module uim.logging.classes.engines.memory;
 
 import uim.logging;
 
 /**
- * Array Log engine.
+ * Memory Log engine.
  *
  * Collects log messages in memory. Intended primarily for usage
  * in testing where using mocks would be complicated. But can also
  * be used in scenarios where you need to capture logs in application code.
  */
 @safe:
-class DArrayLogEngine : DLogEngine {
-  mixin(LogEngineThis!("Array"));
+class DMemoryLogEngine : DLogEngine {
+  mixin(LogEngineThis!("Memory"));
 
   override bool initialize(IData[string] initData = null) {
 if (!super.initialize(configSettings)) {
@@ -21,4 +21,4 @@ return false;
 return true;
   }
 }
-mixin(LogEngineCalls!("Array"));
+mixin(LogEngineCalls!("Memory"));

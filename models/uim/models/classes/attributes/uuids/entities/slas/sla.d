1e5c5f72599bd534959146f17ad1f5c07100e9b7
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-10 19:48:31 +0200

    Cleaning

--- models/uim/models/classes/attributes/uuids/entities/slas/sla.d
+++ models/uim/models/classes/attributes/uuids/entities/slas/sla.d
@@ -3,40 +3,38 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.models.classes.attributes.uuids.entities.users.createdonbehalfby;
+module models.uim.models.classes.attributes.uuids.entities.slas.sla;
 
 import uim.models;
 
 @safe:
-class DCreatedOnBehalfByAttribute : DEntityIdAttribute {
-  mixin(AttributeThis!("CreatedOnBehalfBy"));
+class DSlaIdAttribute : DEntityIdAttribute {
+  mixin(AttributeThis!("SlaId"));
 
   // Initialization hook method.
   override bool initialize(Json[string] initData = null) {
-    if (!super.initialize(initData)) {
-      return false;
-    }
+    if (!super.initialize(initData)) { return false; }
 
-    name("createdOnBehalfBy");
-    registerPath("createdOnBehalfBy");
+    name("slaId");
+    registerPath("slaId");
 
-    return true;
-  }
+   return true;
+
+  }  
 }
-
-mixin(AttributeCalls!("CreatedOnBehalfBy"));
+mixin(AttributeCalls!("SlaId"));
 
 ///
 unittest {
-  auto attribute = new DCreatedOnBehalfByAttribute;
-  assert(attribute.name == "createdOnBehalfBy");
-  assert(attribute.registerPath == "createdOnBehalfBy");
+  auto attribute = new DSlaIdAttribute;
+  assert(attribute.name == "slaId");
+  assert(attribute.registerPath == "slaId");
 
   DAttribute generalAttribute = attribute;
-  assert(cast(DEntityIdAttribute) generalAttribute);
-  assert(cast(DUUIDAttribute) generalAttribute);
-  assert(!cast(DIntegerAttribute) generalAttribute);
+  assert(cast(DEntityIdAttribute)generalAttribute);
+  assert(cast(DUUIDAttribute)generalAttribute);
+  assert(!cast(DIntegerAttribute)generalAttribute);
 
   // Json value = attribute.createValue();
-  // assert(cast(DUUIDData) value);
+  // assert(cast(DUUIDData)value);
 }

e83400c304fbb76376f06e85c1a03a30d1a6a28a
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-15 07:11:46 +0200

    Cleaning

--- oop/uim/oop/base/package.d
+++ oop/uim/oop/base/package.d
@@ -1,6 +1,10 @@
 /***********************************************************************************
-*	Copyright: © 2015-2024 Ozan Nurettin Süel (sicherheitsschmiede)                   *
+*	Copyright: © 2015-2024 Ozan Nurettin Süel (sicherheitsschmiede)                  *
 *	License  : Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt] *
 *	Author   : Ozan Nurettin Süel (Sicherheitsschmiede)										           * 
 ***********************************************************************************/
 module uim.oop.base;
+
+public {
+
+}

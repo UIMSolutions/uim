d9076dc97a155659a92e203bf6ffa86c8e4f20d6
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-15 07:18:18 +0200

    Bugfixing

--- oop/uim/oop/configurations/package.d
+++ oop/uim/oop/configurations/package.d
@@ -4,17 +4,17 @@ public { // Packages
     import uim.oop.configurations.engines;
 }
 public { // Modules
-    import uim.oop.configurations.config;
-    import uim.oop.configurations.configurable;
-    import uim.oop.configurations.configuration;
-    import uim.oop.configurations.file;
-    import uim.oop.configurations.ini;
-    import uim.oop.configurations.interface_;
-    import uim.oop.configurations.json;
-    import uim.oop.configurations.memory;
-    import uim.oop.configurations.mixin_;
-    import uim.oop.configurations.xml;
-    import uim.oop.configurations.yaml;
+    // import uim.oop.configurations.config;
+    // import uim.oop.configurations.configurable;
+    // import uim.oop.configurations.configuration;
+    // import uim.oop.configurations.file;
+    // import uim.oop.configurations.ini;
+    // import uim.oop.configurations.interface_;
+    // import uim.oop.configurations.json;
+    // import uim.oop.configurations.memory;
+    // import uim.oop.configurations.mixin_;
+    // import uim.oop.configurations.xml;
+    // import uim.oop.configurations.yaml;
     
-    import uim.oop.configurations.tests;
+    // import uim.oop.configurations.tests;
 }

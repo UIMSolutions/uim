04376346401b4f6710fb2755d5c183e4ad269772
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-19 07:41:57 +0200

    Cleaning

---
+++ models/uim/models/classes/data/arrays/package.d
@@ -0,0 +1,17 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2024 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.models.datatypes.arrays;
+
+public { 
+	import uim.models.datatypes.arrays.array_;
+}
+
+public { 
+	//import uim.models.datatypes.arrays.integer;
+	//import uim.models.datatypes.arrays.string_;
+	//import uim.models.datatypes.arrays.tag;
+	//import uim.models.datatypes.arrays.value;
+}
--- models/uim/models/classes/data/package.d
+++ models/uim/models/classes/data/package.d
@@ -26,3 +26,17 @@ public {
 	import uim.models.classes.data.entity;
 	import uim.models.classes.data.json; 
 }
+public { // packages
+    import uim.models.datatypes.arrays;
+    import uim.models.datatypes.datetimes;
+    import uim.models.datatypes.maps;
+    import uim.models.datatypes.scalars;
+}
+
+public { // Modules
+    import uim.models.datatypes.data;
+    import uim.models.datatypes.helper;
+    import uim.models.datatypes.interface_;
+    import uim.models.datatypes.null_;
+    import uim.models.datatypes.mixin_;
+}

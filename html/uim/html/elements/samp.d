00bb088f5ada9db49aba6fd0223ba54dc36e9803
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:22:09 +0200

    Cleaning

---
+++ html/uim/html/elements/samp.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.samp;
+
+@safe:
+import uim.html;
+
+class DH5Samp : DH5Obj {
+	mixin(H5This!"samp");
+}
+mixin(H5Short!"Samp");
+
+version(test_uim_html) { unittest {
+    assert(H5Samp,"<samp></samp>");
+}}

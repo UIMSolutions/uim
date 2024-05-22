16d6419845f937ae0dac9a82e435e283964f8cec
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:23:20 +0200

    Cleaning

---
+++ html/uim/html/snippets/package.d
@@ -0,0 +1,9 @@
+module uim.html.snippets;
+
+@safe:
+import uim.html;
+
+public import uim.html.snippets.clientOrPartnersLogoSlider;
+public import uim.html.snippets.dynamicSortableTable;
+public import uim.html.snippets.multiItemCarousel;
+public import uim.html.snippets.productShoppingGridStyles;

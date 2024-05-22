16748b2d0289262c82906f7a6c8fc4fb5465d561
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:28:47 +0200

    Cleaning

---
+++ html/uim/html/classes/elements/element.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.element;
+
+@safe:
+import uim.html;
+
+class DH5Element : DH5Obj {
+	mixin(H5This!"element");
+}
+mixin(H5Short!"Element");
+
+version(test_uim_html) { unittest {
+	assert(H5Element, "element");
+}}
---
+++ html/uim/html/classes/elements/em.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.em;
+
+@safe:
+import uim.html;
+
+class DH5Em: DH5Obj {
+	mixin(H5This!"em");
+}
+mixin(H5Short!"Em");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Em, "em");
+}}

4a36d8703e1344a00b1a082ee955b7ef59e8de71
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:22:18 +0200

    Cleaning

---
+++ html/uim/html/elements/sub.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.sub;
+
+@safe:
+import uim.html;
+
+class DH5Sub : DH5Obj {
+	mixin(H5This!"sub");
+}
+mixin(H5Short!"Sub");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Sub, "sub");
+}}

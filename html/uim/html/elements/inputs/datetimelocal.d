28338cc43dffd8299c748732c68260fbc333d936
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:22:53 +0200

    Cleaning

---
+++ html/uim/html/elements/inputs/datetimelocal.d
@@ -0,0 +1,10 @@
+﻿module uim.html.elements.inputs.datetimelocal;
+
+@safe:
+import uim.html;
+
+class DH5InputDATETIMELOCAL : DH5Input {
+	mixin(H5This!("Input", null, `["type":"datetime-local"]`)); 
+}
+mixin(H5Short!"InputDATETIMELOCAL");
+

d318afa3e54cd7085923fc1512ce08a83a087a75
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:21:53 +0200

    Cleaning

---
+++ html/uim/html/elements/menu.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.menu;
+
+@safe:
+import uim.html;
+
+class DH5Menu : DH5Obj {
+	mixin(H5This!"menu");
+}
+mixin(H5Short!"Menu");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Menu, "menu");
+}}

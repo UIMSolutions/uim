a7dc335909daf4c613307ba9cff80061d7e62970
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:29:28 +0200

    Adding

---
+++ html/uim/html/classes/elements/param.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.param;
+
+@safe:
+import uim.html;
+
+class DH5Param : DH5Obj {
+	mixin(H5This!"param");
+}
+mixin(H5Short!"Param");
+
+version(test_uim_html) { unittest {
+    testH5Obj(H5Param, "param");
+}}

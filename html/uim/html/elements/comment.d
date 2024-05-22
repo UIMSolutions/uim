356c23611ca2bca9b995872f0adfac2d68d0a6ae
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:21:19 +0200

    Cleaning

---
+++ html/uim/html/elements/comment.d
@@ -0,0 +1,20 @@
+﻿module uim.html.elements.comment;
+
+@safe:
+import uim.html;
+
+class DH5Comment : DH5Obj {
+	mixin(H5This!"comment");
+
+	override string toString() {
+//		auto content = "";
+//		foreach(obj; objects) content ~= obj.toString;
+//		return   "<!-- %s -->".format(content);
+		return super.toString;
+	}
+}
+mixin(H5Short!"Comment");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Comment, "comment");
+}}
---
+++ html/uim/html/elements/details.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.details;
+
+@safe:
+import uim.html;
+
+class DH5Details: DH5Obj {
+	mixin(H5This!"details");
+}
+mixin(H5Short!"Details");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Details, "details");
+}}

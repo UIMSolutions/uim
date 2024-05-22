801e312a4b31a3f3c2c4fa92cd8f00846b43ecb1
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:21:44 +0200

    Cleaning

---
+++ html/uim/html/elements/iframe.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.iframe;
+
+@safe:
+import uim.html;
+
+class DH5Iframe : DH5Obj {
+	mixin(H5This!"iframe");
+}
+mixin(H5Short!"Iframe");
+
+version(test_uim_html) { unittest {
+    assert(H5Iframe == "<iframe></iframe>");
+}}

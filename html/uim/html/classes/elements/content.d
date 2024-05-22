368b1a6e0bf298b2824ce8653b12b2c2020e640f
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:28:39 +0200

    Cleaning

---
+++ html/uim/html/classes/elements/content.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.content;
+
+@safe:
+import uim.html;
+
+class DH5Content : DH5Obj {
+	mixin(H5This!("content"));
+}
+mixin(H5Short!"Content");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Content, "content");
+}}

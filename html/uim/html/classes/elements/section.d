b17d192703d61f5058ccc61fdddd41976c16e93c
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:29:44 +0200

    Adding

---
+++ html/uim/html/classes/elements/section.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.section;
+
+@safe:
+import uim.html;
+
+class DH5Section : DH5Obj {
+	mixin(H5This!("section"));
+}
+mixin(H5Short!"Section");
+
+version(test_uim_html) { unittest {
+    testH5Obj(H5Section, "section");
+}}
---
+++ html/uim/html/classes/elements/span.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.span;
+
+@safe:
+import uim.html;
+
+class DH5Span : DH5Obj {
+	mixin(H5This!"Span");
+}
+mixin(H5Short!"Span");
+
+version(test_uim_html) { unittest {	
+	assert(H5Span == "<span></span>");
+}}

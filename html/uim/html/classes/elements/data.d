f90babbc9871290476227c035667f294c38a3ec1
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:28:58 +0200

    Cleaning

---
+++ html/uim/html/classes/elements/data.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.data;
+
+@safe:
+import uim.html;
+
+class DH5Data : DH5Obj {
+	mixin(H5This!"data");
+}
+mixin(H5Short!"Data");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Data, "data");
+}}
---
+++ html/uim/html/classes/elements/footer.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.footer;
+
+@safe:
+import uim.html;
+
+class DH5Footer : DH5Obj {
+	mixin(H5This!("Footer"));
+}
+mixin(H5Short!"Footer");
+
+version(test_uim_html) { unittest {	
+	testH5Obj(H5Footer, "footer");
+}}

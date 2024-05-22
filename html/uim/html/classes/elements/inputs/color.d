bdb152f3f419fe202cffba2e14b54c0caefccfb7
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:30:45 +0200

    Adding

---
+++ html/uim/html/classes/elements/inputs/color.d
@@ -0,0 +1,17 @@
+﻿module uim.html.elements.inputs.color;
+
+@safe:
+import uim.html;
+
+class DH5InputCOLOR : DH5Input {
+	mixin(H5This!""); 
+
+	override string toString() {
+		this["TYPE"] = "color";
+		return super.toString;
+	}
+}
+
+version(test_uim_html) { unittest {
+		// TODO Add Test
+		}}

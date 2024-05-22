d2384c289a9639b107a41bd6acbe755d5ef1dd5f
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:21:26 +0200

    Cleaning

---
+++ html/uim/html/elements/dt.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.dt;
+
+@safe:
+import uim.html;
+
+class DH5Dt : DH5Obj {
+	mixin(H5This!"dt");
+}
+mixin(H5Short!"Dt");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Dt, "dt");
+}}

d128d6a52306bedf0b1c0b9d5755efa5266acdb0
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:21:37 +0200

    Cleaning

---
+++ html/uim/html/elements/kbd.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.kbd;
+
+@safe:
+import uim.html;
+
+class DH5Kbd : DH5Obj {
+	mixin(H5This!"kbd");
+}
+mixin(H5Short!"Kbd");
+
+version(test_uim_html) { unittest {
+  testH5Obj(H5Kbd, "kbd");
+}}

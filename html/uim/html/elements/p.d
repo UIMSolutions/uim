af636b4ec50b469bc2f40abfa3e0f2d96747decf
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:21:58 +0200

    Cleaning

---
+++ html/uim/html/elements/p.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.p;
+
+@safe:
+import uim.html;
+
+class DH5P : DH5Obj {
+	mixin(H5This!("P"));
+}
+mixin(H5Short!"P");
+
+version(test_uim_html) { unittest {
+    assert(H5P == "<p></p>");
+}}
---
+++ html/uim/html/elements/rbc.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.rbc;
+
+@safe:
+import uim.html;
+
+class DH5Rbc : DH5Obj {
+	mixin(H5This!"rbc");
+}
+mixin(H5Short!"Rbc");
+
+version(test_uim_html) { unittest {
+    assert(H5Rbc == "<rbc></rbc>");
+}}

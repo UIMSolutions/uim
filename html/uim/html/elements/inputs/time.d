67a187055cbd36476cc6a18bac3cf6d9720518f8
UIMSolutions <ui.manufaktur@gmail.com>
2024-05-22 21:23:01 +0200

    Cleaning

---
+++ html/uim/html/elements/inputs/time.d
@@ -0,0 +1,13 @@
+﻿module uim.html.elements.inputs.time;
+
+@safe:
+import uim.html;
+
+class DH5InputTIME : DH5Input {
+	mixin(H5This!("Input", null, `["type":"time"]`, true)); 
+}
+mixin(H5Short!"InputTIME"); 
+
+version(test_uim_html) { unittest {
+		// TODO Add Test
+		}}

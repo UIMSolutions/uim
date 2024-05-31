d0b7cf61da8006132df6b7b7e92bb2da84194d1b
Business in D <ui.manufaktur@gmail.com>
2024-05-31 16:59:48 +0200

    Aktualisieren von i18n.d

--- commands/uim/commands/classes/commands/i18n/i18n.d
+++ commands/uim/commands/classes/commands/i18n/i18n.d
@@ -16,36 +16,35 @@ class DI18nCommand : DCommand {
         return true;
     }
 
-    /*
     // Execute interactive mode
-    int execute(Json[string] arguments, IConsoleIo aConsoleIo) {
-        aConsoleIo.writeln("<info>I18n Command</info>");
-        aConsoleIo.hr();
-        aConsoleIo.writeln("[E]xtract POT file from sources");
-        aConsoleIo.writeln("[I]nitialize a language from POT file");
-        aConsoleIo.writeln("[H]elp");
-        aConsoleIo.writeln("[Q]uit");
+    int execute(Json[string] arguments, IConsoleIo consoleIo) {
+        consoleIo.writeln("<info>I18n Command</info>");
+        consoleIo.hr();
+        consoleIo.writeln("[E]xtract POT file from sources");
+        consoleIo.writeln("[I]nitialize a language from POT file");
+        consoleIo.writeln("[H]elp");
+        consoleIo.writeln("[Q]uit");
 
         do {
-            string choice = aConsoleIo.askChoice("What would you like to do?", ["E", "I", "H", "Q"])
+            string choice = consoleIo.askChoice("What would you like to do?", ["E", "I", "H", "Q"])
                 .lower;
-            code = null;
+            auto code = null;
             switch (choice) {
-            case "e" : code = executeCommand(I18nExtractCommand.classname, [], aConsoleIo);
+            case "e" : code = executeCommand(I18nExtractCommand.classname, [], consoleIo);
                 break;
-            case "i" : code = executeCommand(I18nInitCommand.classname, [], aConsoleIo);
+            case "i" : code = executeCommand(I18nInitCommand.classname, [], consoleIo);
                 break;
-            case "h" : aConsoleIo.writeln(getOptionParser().help());
+            case "h" : consoleIo.writeln(getOptionParser().help());
                 break;
             case "q" : // Do nothing
                 break;
-            default : aConsoleIo.writeErrorMessages(
+            default : consoleIo.writeErrorMessages(
                     "You have made an invalid selection. " ~
                         "Please choose a command to execute by entering E, I, H, or Q."
                 );
             }
             if (code == CODE_ERROR) {
-                this.abort();
+                abort();
             }
         }
         while (choice != "q");
@@ -60,7 +59,7 @@ class DI18nCommand : DCommand {
         );
 
         return aParser;
-    } */
+    } 
 }
 
 mixin(CommandCalls!("I18n"));

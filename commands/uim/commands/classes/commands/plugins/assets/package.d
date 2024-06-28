0b4a5ca9b5e640cb7a67a7aee6bfde15891a48ad
UIMSolutions <ui.manufaktur@gmail.com>
2024-06-28 16:18:31 +0200

    Working on commands

--- commands/uim/commands/classes/commands/plugins/assets/package.d
+++
@@ -1,7 +0,0 @@
-module uim.commands.classes.commands.plugins.assets;
-
-public {
-    import uim.commands.classes.commands.plugins.assets.copy;
-    import uim.commands.classes.commands.plugins.assets.remove;
-    import uim.commands.classes.commands.plugins.assets.symlink;
-}
--- commands/uim/commands/classes/commands/routes/package.d
+++
@@ -1,7 +0,0 @@
-module uim.commands.classes.commands.routes;
-
-public {
-    import uim.commands.classes.commands.routes.routes;
-    import uim.commands.classes.commands.routes.check;
-    import uim.commands.classes.commands.routes.generate;
-}

e8661d82c854d19ee6e6c5f2f461dc6adc7feb9e
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-02 06:43:54 +0200

    Daily bugfixing

--- models/uim/models/classes/attributes/doubles/joule.d
+++ models/uim/models/classes/attributes/doubles/joule.d
@@ -3,4 +3,40 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.models.classes.attributes.doubles.joule;    if (!super.initialize(initData)) {      return false;    }/* Unit of measure for energy, work or heat in joulesInheritanceany <- float <- double <- jouleTraitsis.dataFormat.floatingPointis.dataFormat.bigmeans.measurement.dimension.energymeans.measurement.units.si.joulehas.measurement.fundamentalComponent.kilogramhas.measurement.fundamentalComponent.meterhas.measurement.fundamentalComponent.second */import uim.models;@safe:class DJouleAttribute : DDoubleAttribute {  mixin(AttributeThis!("Joule"));  // Initialization hook method.  override bool initialize(Json[string] initData = null) {    if (!super.initialize(initData)) { return false; }    name("joule");     registerPath("joule");    return true;  }}mixin(AttributeCalls!("Joule"));version(test_uim_models) { unittest {    // TODO  }}
+module uim.models.classes.attributes.doubles.joule;   
+
+/* Unit of measure for energy, work or heat in joules
+
+Inheritance
+any <- float <- double <- joule
+Traits
+is.dataFormat.floatingPoint
+is.dataFormat.big
+means.measurement.dimension.energy
+means.measurement.units.si.joule
+has.measurement.fundamentalComponent.kilogram
+has.measurement.fundamentalComponent.meter
+has.measurement.fundamentalComponent.second */
+
+import uim.models;
+
+@safe:
+class DJouleAttribute : DDoubleAttribute {
+  mixin(AttributeThis!("Joule"));
+
+  // Initialization hook method.
+  override bool initialize(Json[string] initData = null) {
+    if (!super.initialize(initData)) { return false; }
+
+    name("joule"); 
+    registerPath("joule");
+
+    return true;
+  }
+}
+mixin(AttributeCalls!("Joule"));
+
+version(test_uim_models) { unittest {
+    // TODO
+  }
+}
--- models/uim/models/classes/attributes/elements/element.d
+++ models/uim/models/classes/attributes/elements/element.d
@@ -3,4 +3,28 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.models.classes.attributes.elements.element;    if (!super.initialize(initData)) {      return false;    }import uim.models;@safe:class DElementAttribute : DAttribute {  mixin(AttributeThis!("Element"));  // Initialization hook method.  override bool initialize(Json[string] initData = null) {    if (!super.initialize(initData)) { return false; }    // TODO     return true;  }  /* override Json createData() {    return ElementData(this); } */}mixin(AttributeCalls!"Element");version(test_uim_models) { unittest {  testAttribute(new Attribute);  testAttribute(ElementAttribute);}}
+module uim.models.classes.attributes.elements.element;    
+
+import uim.models;
+
+@safe:
+class DElementAttribute : DAttribute {
+  mixin(AttributeThis!("Element"));
+
+  // Initialization hook method.
+  override bool initialize(Json[string] initData = null) {
+    if (!super.initialize(initData)) { return false; }
+
+    // TODO 
+    return true;
+  }
+
+  /* override Json createData() {
+    return ElementData(this); } */
+}
+mixin(AttributeCalls!"Element");
+
+version(test_uim_models) { unittest {
+  testAttribute(new Attribute);
+  testAttribute(ElementAttribute);
+}}

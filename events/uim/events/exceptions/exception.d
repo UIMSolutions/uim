39d6e80a7dad307df2ce83a1bce185446eadd298
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-05 08:00:00 +0200

    Rename DException to UIMException

--- events/uim/events/exceptions/exception.d
+++ events/uim/events/exceptions/exception.d
@@ -3,4 +3,25 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.events.exceptions.exception;import uim.events;@safe:// Base events exception.class DEventsException : DException {  mixin(ExceptionThis!("Events"));  override bool initialize(Json[string] initData = null) {    if (!super.initialize(initData)) {      return false;    }    this      .messageTemplate("Error in libary uim-events");    return true;  }}mixin(ExceptionCalls!("Events"));
+module uim.events.exceptions.exception;UIMException
+
+import uim.events;
+
+@safe:
+
+// Base events exception.
+class DEventsException : DException {
+  mixin(ExceptionThis!("Events"));
+
+  override bool initialize(Json[string] initData = null) {
+    if (!super.initialize(initData)) {
+      return false;
+    }
+
+    this
+      .messageTemplate("Error in libary uim-events");
+
+    return true;
+  }
+}
+mixin(ExceptionCalls!("Events"));
--- i18n/uim/i18n/exceptions/exception.d
+++ i18n/uim/i18n/exceptions/exception.d
@@ -3,4 +3,25 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.i18n.exceptions.exception;import uim.i18n;@safe:// I18n exception.class DI18nException : DException {  mixin(ExceptionThis!("I18n"));  override bool initialize(Json[string] initData = null) {    if (!super.initialize(initData)) {      return false;    }    this      .messageTemplate("Error in libary uim-i18n");    return true;  }}mixin(ExceptionCalls!("I18n"));
+module uim.i18n.exceptions.exception;UIMException
+
+import uim.i18n;
+
+@safe:
+
+// I18n exception.
+class DI18nException : DException {
+  mixin(ExceptionThis!("I18n"));
+
+  override bool initialize(Json[string] initData = null) {
+    if (!super.initialize(initData)) {
+      return false;
+    }
+
+    this
+      .messageTemplate("Error in libary uim-i18n");
+
+    return true;
+  }
+}
+mixin(ExceptionCalls!("I18n"));

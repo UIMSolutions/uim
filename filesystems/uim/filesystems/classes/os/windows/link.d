0a64a5a085cd4943112902efeba3b2fba6fbeda2
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-05 17:15:04 +0200

    Code cleaning

--- filesystems/uim/filesystems/classes/os/windows/link.d
+++ filesystems/uim/filesystems/classes/os/windows/link.d
@@ -3,4 +3,19 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.filesystems.classes.os.windows.link;import uim.filesystems;unittest {   version(testUimFilesystems) {     debug writeln("\n", __MODULE__~": "~__PRETTY_FUNCTION__);   }}@safe:class DWindowsLink : DLink {  mixin(LinkThis!("Windows"));}mixin(LinkCalls!("Windows"));
+module uim.filesystems.classes.os.windows.link;
+
+import uim.filesystems;
+
+unittest {
+  version (testUimFilesystems) {
+    debug writeln("\n", __MODULE__ ~ ": " ~ __PRETTY_FUNCTION__);
+  }
+}
+
+@safe:
+class DWindowsLink : DLink {
+  mixin(LinkThis!("Windows"));
+}
+
+mixin(LinkCalls!("Windows"));

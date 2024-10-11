277011f902f67169a97f25fdf8f80b0f6e7f72ba
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-11 07:17:03 +0200

    Code cleaning

--- http/uim/http/classes/adapters/curl.d
+++ http/uim/http/classes/adapters/curl.d
@@ -3,4 +3,161 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.http.classes.adapters.curl;import uim.http;@safe:/** * : sending UIM\Http\Client\Request via ext/curl. * * In addition to the standard curlOptions documented in {@link \UIM\Http\Client}, * this adapter supports all available curl curlOptions. Additional curl curlOptions * can be set via the `curl` option key when making requests or configuring * a client. */class DCurl { // }: IAdapter {    /*    Json[string] send(IRequest request, Json[string] curlOptions = null) {        if (!extension_loaded("curl")) {            throw new DClientException("curl extension is not loaded.");        }        ch = curl_initialize();        curlOptions = this.buildOptions(request, curlOptions);        curl_setopt_array(ch, curlOptions);        body = this.exec(ch);        assert(body != true);        if (body == false) {            errorCode = curl_errno(ch);            error = curl_error(ch);            curl_close(ch);            string message = "cURL Error ({errorCode}) {error}";            errorNumbers = [                CURLE_FAILED_INIT,                CURLE_URL_MALFORMAT,                CURLE_URL_MALFORMAT_USER,            ];            if (isIn(errorCode, errorNumbers, true)) {                throw new DRequestException(message, request);            }            throw new DNetworkException(message, request);        }        responses = this.createResponse(ch, body);        curl_close(ch);        return responses;    }        // Convert client options into curl options.    Json[string] buildOptions(IRequest request, Json[string] clientOptions = null) {        string[] aHeaders = request.getHeaders().byKeyValue            .map!(keyValues => aKey ~ ": " ~ someValues.join(", ")).array;         result = [            CURLOPT_URL: (string)request.getUri(),            CURLOPT_HTTP_VERSION: getProtocolVersion(request),            CURLOPT_RETURNTRANSFER: true,            CURLOPT_HEADER: true,            CURLOPT_HTTPHEADER:  aHeaders,        ];        switch (request.getMethod()) {            case Request.METHOD_GET:                 t(CURLOPT_HTTPGET, true);                break;            case Request.METHOD_POST:                 t(CURLOPT_POST, true);                break;            case Request.METHOD_HEAD:                 t(CURLOPT_NOBODY, true);                break;            default:                 t(CURLOPT_POST, true);                 t(CURLOPT_CUSTOMREQUEST, request.getMethod());                break;        }        body = request.getBody();        body.rewind();         t(CURLOPT_POSTFIELDS, body.getContents());        // GET requests with bodies require custom request to be used.        if (result[CURLOPT_POSTFIELDS] != "" && result.hasKey(CURLOPT_HTTPGET))) {            result.set(URLOPT_CUSTOMREQUEST, "get");        }        if (result.isEmpty(CURLOPT_POSTFIELDS)) {            result.removeKey(CURLOPT_POSTFIELDS);        }        if (clientOptions.isEmpty("ssl_cafile")) {            clientOptions.set("ssl_cafile", CaBundle.getBundledCaBundlePath());        }        if (!clientOptions.isEmpty("ssl_verify_host"))) {            // Value of 1 or true is deprecated. Only 2 or 0 should be used now.            clientoptions.get("ssl_verify_host"] = 2;        }        optionMap = [            "timeout": CURLOPT_TIMEOUT,            "ssl_verify_peer": CURLOPT_SSL_VERIFYPEER,            "ssl_verify_host": CURLOPT_SSL_VERIFYHOST,            "ssl_cafile": CURLOPT_CAINFO,            "ssl_local_cert": CURLOPT_SSLCERT,            "ssl_passphrase": CURLOPT_SSLCERTPASSWD,        ];                optionMap.byKeyValue            .filter!(optionCurlOpt => clientOptions.hasKey(optionCurlOpt.key))            .each!(optionCurlOpt => result[optionCurlOpt.value] = clientOptions.get(optionCurlOpt.key));                    if (clientOptions.hasKey("proxy.proxy")) {             result[CURLOPT_PROXY] = clientoptions.get("proxy.proxy"];        }        if (clientOptions.hasKey("proxy.username")) {            password = !clientOptions.isEmpty("proxy.password") ? clientoptions.get("proxy.password"] : "";             result[CURLOPT_PROXYUSERPWD] = clientOptions.getString("proxy.username") ~ ": " ~ password;        }        if (clientOptions.hasKey("curl") && clientoptions.isArray("curl"]) {            // Can`t use array_merge() because keys will be re-ordered.            clientoptions.get("curl"].byKeyValue                .each!(kv => result.set(kv.key, kv.value));        }        return result;    }        // Convert HTTP version number into curl value.    protected int getProtocolVersion(IRequest request) {        /* return match (request.getProtocolVersion()) {            "1.0": CURL_HTTP_VERSION_1_0,            "1.1": CURL_HTTP_VERSION_1_1,            "2", "2.0": defined("CURL_HTTP_VERSION_2TLS")                ? CURL_HTTP_VERSION_2TLS                : (defined("CURL_HTTP_VERSION_2_0")                    ? CURL_HTTP_VERSION_2_0                    : throw new DHttpException("libcurl 7.33 or greater required for HTTP/2 support")               ),            default: CURL_HTTP_VERSION_NONE,        }; */        return 0;    }        // Convert the raw curl response into an Http\Client\Response    protected DResponse[] createResponse(CurlHandle handle, string responseData) {        auto aHeaderSize = curl_getinfo(handle, CURLINFO_HEADER_SIZE);        auto aHeaders = subString(responseData, 0,  aHeaderSize).strip;        body = subString(responseData,  aHeaderSize);        auto response = new DResponse(split("\r\n",  aHeaders), body);        return [response];    }        /**     * Execute the curl handle.     * Params:     * \CurlHandle ch Curl Resource handle     */    protected string exec(CurlHandle ch) {        return curl_exec(ch);    }}
+module uim.http.classes.adapters.curl;
+
+import uim.http;
+
+@safe:
+
+/**
+ * : sending UIM\Http\Client\Request via ext/curl.
+ *
+ * In addition to the standard curlOptions documented in {@link \UIM\Http\Client},
+ * this adapter supports all available curl curlOptions. Additional curl curlOptions
+ * can be set via the `curl` option key when making requests or configuring
+ * a client.
+ */
+class DCurl { // }: IAdapter {
+    /*
+    Json[string] send(IRequest request, Json[string] curlOptions = null) {
+        if (!extension_loaded("curl")) {
+            throw new DClientException("curl extension is not loaded.");
+        }
+        ch = curl_initialize();
+        curlOptions = this.buildOptions(request, curlOptions);
+        curl_setopt_array(ch, curlOptions);
+
+        body = this.exec(ch);
+        assert(body != true);
+        if (body == false) {
+            errorCode = curl_errno(ch);
+            error = curl_error(ch);
+            curl_close(ch);
+
+            string message = "cURL Error ({errorCode}) {error}";
+            errorNumbers = [
+                CURLE_FAILED_INIT,
+                CURLE_URL_MALFORMAT,
+                CURLE_URL_MALFORMAT_USER,
+            ];
+            if (isIn(errorCode, errorNumbers, true)) {
+                throw new DRequestException(message, request);
+            }
+            throw new DNetworkException(message, request);
+        }
+        responses = this.createResponse(ch, body);
+        curl_close(ch);
+
+        return responses;
+    }
+    
+    // Convert client options into curl options.
+    Json[string] buildOptions(IRequest request, Json[string] clientOptions = null) {
+        string[] aHeaders = request.getHeaders().byKeyValue
+            .map!(keyValues => aKey ~ ": " ~ someValues.join(", ")).array;
+
+         result = [
+            CURLOPT_URL: (string)request.getUri(),
+            CURLOPT_HTTP_VERSION: getProtocolVersion(request),
+            CURLOPT_RETURNTRANSFER: true,
+            CURLOPT_HEADER: true,
+            CURLOPT_HTTPHEADER:  aHeaders,
+        ];
+        switch (request.getMethod()) {
+            case Request.METHOD_GET:
+                 t(CURLOPT_HTTPGET, true);
+                break;
+
+            case Request.METHOD_POST:
+                 t(CURLOPT_POST, true);
+                break;
+
+            case Request.METHOD_HEAD:
+                 t(CURLOPT_NOBODY, true);
+                break;
+
+            default:
+                 t(CURLOPT_POST, true);
+                 t(CURLOPT_CUSTOMREQUEST, request.getMethod());
+                break;
+        }
+        body = request.getBody();
+        body.rewind();
+         t(CURLOPT_POSTFIELDS, body.getContents());
+        // GET requests with bodies require custom request to be used.
+        if (result[CURLOPT_POSTFIELDS] != "" && result.hasKey(CURLOPT_HTTPGET))) {
+            result.set(URLOPT_CUSTOMREQUEST, "get");
+        }
+        if (result.isEmpty(CURLOPT_POSTFIELDS)) {
+            result.removeKey(CURLOPT_POSTFIELDS);
+        }
+        if (clientOptions.isEmpty("ssl_cafile")) {
+            clientOptions.set("ssl_cafile", CaBundle.getBundledCaBundlePath());
+        }
+        if (!clientOptions.isEmpty("ssl_verify_host"))) {
+            // Value of 1 or true is deprecated. Only 2 or 0 should be used now.
+            clientoptions.get("ssl_verify_host"] = 2;
+        }
+        optionMap = [
+            "timeout": CURLOPT_TIMEOUT,
+            "ssl_verify_peer": CURLOPT_SSL_VERIFYPEER,
+            "ssl_verify_host": CURLOPT_SSL_VERIFYHOST,
+            "ssl_cafile": CURLOPT_CAINFO,
+            "ssl_local_cert": CURLOPT_SSLCERT,
+            "ssl_passphrase": CURLOPT_SSLCERTPASSWD,
+        ];
+        
+        optionMap.byKeyValue
+            .filter!(optionCurlOpt => clientOptions.hasKey(optionCurlOpt.key))
+            .each!(optionCurlOpt => result[optionCurlOpt.value] = clientOptions.get(optionCurlOpt.key));
+            
+        if (clientOptions.hasKey("proxy.proxy")) {
+             result[CURLOPT_PROXY] = clientoptions.get("proxy.proxy"];
+        }
+        if (clientOptions.hasKey("proxy.username")) {
+            password = !clientOptions.isEmpty("proxy.password") ? clientoptions.get("proxy.password"] : "";
+             result[CURLOPT_PROXYUSERPWD] = clientOptions.getString("proxy.username") ~ ": " ~ password;
+        }
+        if (clientOptions.hasKey("curl") && clientoptions.isArray("curl"]) {
+            // Can`t use array_merge() because keys will be re-ordered.
+            clientoptions.get("curl"].byKeyValue
+                .each!(kv => result.set(kv.key, kv.value));
+
+        }
+        return result;
+    }
+    
+    // Convert HTTP version number into curl value.
+    protected int getProtocolVersion(IRequest request) {
+        /* return match (request.getProtocolVersion()) {
+            "1.0": CURL_HTTP_VERSION_1_0,
+            "1.1": CURL_HTTP_VERSION_1_1,
+            "2", "2.0": defined("CURL_HTTP_VERSION_2TLS")
+                ? CURL_HTTP_VERSION_2TLS
+                : (defined("CURL_HTTP_VERSION_2_0")
+                    ? CURL_HTTP_VERSION_2_0
+                    : throw new DHttpException("libcurl 7.33 or greater required for HTTP/2 support")
+               ),
+            default: CURL_HTTP_VERSION_NONE,
+        }; */
+        /* return 0;
+    } */
+    
+    // Convert the raw curl response into an Http\Client\Response
+    protected DResponse[] createResponse(CurlHandle handle, string responseData) {
+        auto aHeaderSize = curl_getinfo(handle, CURLINFO_HEADER_SIZE);
+        auto aHeaders = subString(responseData, 0,  aHeaderSize).strip;
+        body = subString(responseData,  aHeaderSize);
+        auto response = new DResponse(split("\r\n",  aHeaders), body);
+        return [response];
+    }
+    
+    /**
+     * Execute the curl handle.
+     * Params:
+     * \CurlHandle ch Curl Resource handle
+     */
+    protected string exec(CurlHandle ch) {
+        return curl_exec(ch);
+    }
+}
--- http/uim/http/classes/requests/serverrequestfactory.d
+++ http/uim/http/classes/requests/serverrequestfactory.d
@@ -3,4 +3,130 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.http.classes.requests.serverrequestfactory;import uim.http;@safe:/** * Factory for making ServerRequest instances. * * This adds in UIM specific behavior to populate the basePath and webroot * attributes. Furthermore the Uri`s path is corrected to only contain the * 'virtual' path for the request. */class DServerRequestFactory { // }: ServerIRequestFactory {    /**     * Create a request from the supplied superglobal values.     * If any argument is not supplied, the corresponding superglobal value will be used.     */    static DServerRequest fromGlobals(        Json[string] server = null, // _SERVER superglobal        Json[string] aQuery = null, // _GET superglobal        Json[string] parsedBody = null, // _POST superglobal        Json[string] cookies = null, // _COOKIE superglobal        Json[string] files = null  // _FILES superglobal            ) {        auto server = normalizeServer(server ? server : _SERVER);        ["uri": anUri, "base": base, "webroot": webroot] = UriFactory.marshalUriAndBaseFromSapi(            server);        auto sessionConfig = configuration.getMap("Session")            .merge([                    "defaults": "D",                    "cookiePath": webroot,                ]);        auto session = Session.create(sessionConfig);        auto request = new DServerRequest(            createMap!(string, Json)                .set("environment", server)                .set("uri", anUri)                .set("cookies", cookies ?  ? _COOKIE)                .set("query", aQuery ?  ? _GET)                .set("webroot", webroot)                .set("base", base)                .set("session", session)                .set("input", server.get("uimD_INPUT", null)));        request = marshalBodyAndRequestMethod(parsedBody ? parsedBody : _POST, request);        // This is required as `ServerRequest.scheme()` ignores the value of        // `HTTP_X_FORWARDED_PROTO` unless `trustProxy` is enabled, while the        // `Uri` instance intially created always takes values of `HTTP_X_FORWARDED_PROTO`        // into account.        auto anUri = request.getUri().withScheme(request.scheme());        request = request.withUri(anUri, true);        return marshalFiles(files ? files : _FILES, request);    }    /**     * Sets the REQUEST_METHOD environment variable based on the simulated _method     * HTTP override value. The 'ORIGINAL_REQUEST_METHOD' is also preserved, if you     * want the read the non-simulated HTTP method the client used.     *     * Request body of content type "application/x-www-form-urlencoded" is parsed     * into Json[string] for PUT/PATCH/DELETE requests.     * Params:     * Json[string] parsedBody Parsed body.     */    protected static ServerRequest marshalBodyAndRequestMethod(Json[string] parsedBody, ServerRequest serverRequest) {        method = serverRequest.getMethod();        shouldOverride = false;        if (            isIn(method, ["PUT", "DELETE", "PATCH"], true) &&            (string) serverRequest.contentType().startWith("application/x-www-form-urlencoded")            ) {            someData = (string) serverRequest.getBody();            parse_str(someData, parsedBody);        }        if (serverRequest.hasHeader("X-Http-Method-Override")) {            parsedBody["_method"] = serverRequest.getHeaderLine("X-Http-Method-Override");            shouldOverride = true;        }        serverRequest = serverRequest.withenviroment("ORIGINAL_REQUEST_METHOD", method);        if (parsedBody.hasKey("_method")) {            serverRequest = serverRequest.withenviroment("REQUEST_METHOD", parsedBody["_method"]);            removeKey(parsedBody["_method"]);            shouldOverride = true;        }        return shouldOverride &&            !["PUT", "POST", "DELETE", "PATCH"].has(serverRequest.getMethod())            ? serverRequest.withParsedBody(null) : serverRequest.withParsedBody(parsedBody);    }    // Process uploaded files and move things onto the parsed body.    protected static ServerRequest marshalFiles(Json[string] files, DServerRequest serverRequest) {        auto files = normalizeUploadedFiles(files);        auto serverRequest = serverRequest.withUploadedFiles(files);        auto parsedBody = serverRequest.getParsedBody();        if (!isArray(parsedBody)) {            return serverRequest;        }        return serverRequest.withParsedBody(parsedBody.merge(files));    }    /**     * Create a new server request.     *     * Note that server-params are taken precisely as given - no parsing/processing     * of the given values is performed, and, in particular, no attempt is made to     * determine the HTTP method or URI, which must be provided explicitly.     */    IServerRequest createServerRequest(string httpMethod, string uri, Json[string] serverOptions = null) {        return createServerRequest(httpMethod, (new UriFactory()).createUri(uri), serverOptions);    }    IServerRequest createServerRequest(string httpMethod, IUri uri, Json[string] serverOptions = null) {        serverOptions.set("REQUEST_METHOD", httpMethod);        auto options = createMap!(string. Json)            .set("environment", serverOptions)            .set("uri", uri);        return new DServerRequest(options);    }}
+module uim.http.classes.requests.serverrequestfactory;
+
+import uim.http;
+
+@safe:
+
+/**
+ * Factory for making ServerRequest instances.
+ *
+ * This adds in UIM specific behavior to populate the basePath and webroot
+ * attributes. Furthermore the Uri`s path is corrected to only contain the
+ * 'virtual' path for the request.
+ */
+class DServerRequestFactory { // }: ServerIRequestFactory {
+    /**
+     * Create a request from the supplied superglobal values.
+     * If any argument is not supplied, the corresponding superglobal value will be used.
+     */
+    static DServerRequest fromGlobals(
+        Json[string] server = null, // _SERVER superglobal
+        Json[string] aQuery = null, // _GET superglobal
+        Json[string] parsedBody = null, // _POST superglobal
+        Json[string] cookies = null, // _COOKIE superglobal
+        Json[string] files = null  // _FILES superglobal    
+
+    ) {
+        auto server = normalizeServer(server ? server : _SERVER);
+        ["uri": anUri, "base": base, "webroot": webroot] = UriFactory.marshalUriAndBaseFromSapi(
+            server);
+
+        auto sessionConfig = configuration.getMap("Session")
+            .merge([
+                    "defaults": "D",
+                    "cookiePath": webroot,
+                ]);
+
+        auto session = Session.create(sessionConfig);
+        auto request = new DServerRequest(
+            createMap!(string, Json)
+                .set("environment", server)
+                .set("uri", anUri)
+                .set("cookies", cookies ?  ? _COOKIE)
+                .set("query", aQuery ?  ? _GET)
+                .set("webroot", webroot)
+                .set("base", base)
+                .set("session", session)
+                .set("input", server.get("uimD_INPUT", null)));
+
+        request = marshalBodyAndRequestMethod(parsedBody ? parsedBody : _POST, request);
+        // This is required as `ServerRequest.scheme()` ignores the value of
+        // `HTTP_X_FORWARDED_PROTO` unless `trustProxy` is enabled, while the
+        // `Uri` instance intially created always takes values of `HTTP_X_FORWARDED_PROTO`
+        // into account.
+        auto anUri = request.getUri().withScheme(request.scheme());
+        request = request.withUri(anUri, true);
+
+        return marshalFiles(files ? files : _FILES, request);
+    }
+
+    /**
+     * Sets the REQUEST_METHOD environment variable based on the simulated _method
+     * HTTP override value. The 'ORIGINAL_REQUEST_METHOD' is also preserved, if you
+     * want the read the non-simulated HTTP method the client used.
+     *
+     * Request body of content type "application/x-www-form-urlencoded" is parsed
+     * into Json[string] for PUT/PATCH/DELETE requests.
+     * Params:
+     * Json[string] parsedBody Parsed body.
+     */
+    protected static ServerRequest marshalBodyAndRequestMethod(Json[string] parsedBody, ServerRequest serverRequest) {
+        method = serverRequest.getMethod();
+        shouldOverride = false;
+
+        if (
+            isIn(method, ["PUT", "DELETE", "PATCH"], true) &&
+            (string) serverRequest.contentType().startWith("application/x-www-form-urlencoded")
+            ) {
+            someData = (string) serverRequest.getBody();
+            parse_str(someData, parsedBody);
+        }
+        if (serverRequest.hasHeader("X-Http-Method-Override")) {
+            parsedBody["_method"] = serverRequest.getHeaderLine("X-Http-Method-Override");
+            shouldOverride = true;
+        }
+        serverRequest = serverRequest.withenviroment("ORIGINAL_REQUEST_METHOD", method);
+        if (parsedBody.hasKey("_method")) {
+            serverRequest = serverRequest.withenviroment("REQUEST_METHOD", parsedBody["_method"]);
+            removeKey(parsedBody["_method"]);
+            shouldOverride = true;
+        }
+
+        return shouldOverride &&
+            !["PUT", "POST", "DELETE", "PATCH"].has(serverRequest.getMethod())
+            ? serverRequest.withParsedBody(null) : serverRequest.withParsedBody(parsedBody);
+    }
+
+    // Process uploaded files and move things onto the parsed body.
+    protected static ServerRequest marshalFiles(Json[string] files, DServerRequest serverRequest) {
+        auto files = normalizeUploadedFiles(files);
+        auto serverRequest = serverRequest.withUploadedFiles(files);
+        auto parsedBody = serverRequest.getParsedBody();
+        if (!isArray(parsedBody)) {
+            return serverRequest;
+        }
+        return serverRequest.withParsedBody(parsedBody.merge(files));
+    }
+
+    /**
+     * Create a new server request.
+     *
+     * Note that server-params are taken precisely as given - no parsing/processing
+     * of the given values is performed, and, in particular, no attempt is made to
+     * determine the HTTP method or URI, which must be provided explicitly.
+     */
+    IServerRequest createServerRequest(string httpMethod, string uri, Json[string] serverOptions = null) {
+        return createServerRequest(httpMethod, (new UriFactory()).createUri(uri), serverOptions);
+    }
+
+    IServerRequest createServerRequest(string httpMethod, IUri uri, Json[string] serverOptions = null) {
+        serverOptions.set("REQUEST_METHOD", httpMethod);
+        auto options = createMap!(string.Json)
+            .set("environment", serverOptions)
+            .set("uri", uri);
+
+        return new DServerRequest(options);
+    }
+}

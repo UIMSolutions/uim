8669e3b37bfe23fa45d9f63ea29ef12f530a595e
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-24 15:47:54 +0200

    Code repair

--- http/uim/http/classes/middleware/package.d
+++
@@ -1,6 +0,0 @@
-/****************************************************************************************************************
-* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
-* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
-* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
-*****************************************************************************************************************/
-module uim.http.classes.middleware;public { // Main    import uim.http.classes.middleware;}public {     import uim.http.classes.bodyparser;    import uim.http.classes.closuredecorator;    import uim.http.classes.closuredecorator;}
--- http/uim/http/classes/urifactory.d
+++ http/uim/http/classes/urifactory.d
@@ -24,106 +24,110 @@ class UriFactory { // }: IUriFactory {
      */
   static Json[string] marshalUriAndBaseFromSapi(Json[string] serverData = null) {
     serverData ? serverData : _SERVER;
-    auto azto aHeaders = marshalHeadersFromSapi(serverData);
+    auto aHeaders = marshalHeadersFromSapi(serverData);
 
     auto anUri = DiactorosUriFactory.createFromSapi(serverData, aHeaders);
     ["base": base, "webroot": webroot] = getBase(anUri, serverData);
 
-    // Look in PATH_INFO first, as this is the exact value we need prepared
-    // by D.
+    // Look in PATH_INFO first, as this is the exact value we need prepared by D.
     auto somePathInfo = serverData.get("PATH_INFO", null);
     anUri = somePathInfo
       ? anUri.withPath(somePathInfo) : updatePath(base, anUri);
-  }
 
-  /* if (!anUri.getHost()) {
+    /* if (!anUri.getHost()) {
         anUri = anUri.withHost("localhost");
     } */
 
-  return createMap!(string, Json)
-    .set("uri", "anUri")
-    .set("base", base)
-    .set("webroot", webroot);
-}
-
-// Updates the request URI to remove the base directory.
-protected static IUri updatePath(string basePath, IUri uriToUpdate) {
-  auto uriPath = uriToUpdate.path();
-  if (!basePath.isEmpty && uriPath.startWith(basePath)) {
-    uriPath = uriPath[0 .. basePath.length];
-  }
-  if (uriPath == "/index.d" && uriToUpdate.getQuery()) {
-    uriPath = anUri.getQuery();
-  }
-  if (uriPath.isEmpty || uriPath.isAny("/", "//", "/index.d")) {
-    uriPath = "/";
-  }
-
-  string endsWithIndex = "/" ~ (configuration.get("App.webroot") ?  : "webroot")~"/index.d";
-  size_t endsWithLength = endsWithIndex.length;
-  if (
-    uriPath.length >= endsWithLength &&
-    subString(uriPath, -endsWithLength) == endsWithIndex
-    ) {
-    uriPath = "/";
-  }
-  return anUri.withPath(uriPath);
-}
-
-// Calculate the base directory and webroot directory.
-protected static Json[string] getBase(IUri uri, Json[string] serverData) {
-  auto appData = configuration.getMap("App");
-  configuration.set("App", appData.mergeKeys(["base", "webroot", "baseUrl"]));
-
-  string base = configuration.getString("base");
-  auto baseUrl = configuration.get("baseUrl");
-  string webroot = configuration.getString("webroot");
-
-  if (!base.isNull) {
     return createMap!(string, Json)
-      .set("base", bas);
-    
-    .set("webroot", base.correctUrl);
+      .set("uri", "anUri")
+      .set("base", base)
+      .set("webroot", webroot);
+  }
 
-    if (!baseUrl) {
-      auto self = serverData.get("UIM_SELF");
-      if (DisNull) {
-        return createMap!(string, Json)
-          .set("base", "")
-          .set("webroot", "/");
+  // Updates the request URI to remove the base directory.
+  protected static IUri updatePath(string basePath, IUri uriToUpdate) {
+    auto uriPath = uriToUpdate.path();
+    if (!basePath.isEmpty && uriPath.startWith(basePath)) {
+      uriPath = uriPath[0 .. basePath.length];
+    }
+    if (uriPath == "/index.d" && uriToUpdate.getQuery()) {
+      uriPath = anUri.getQuery();
+    }
+    if (uriPath.isEmpty || uriPath.isAny("/", "//", "/index.d")) {
+      uriPath = "/";
+    }
 
-        base = dirname(serverData.get("UIM_SELF", DIRECTORY_SEPARATOR));
-        // Clean up additional / which cause following code to fail..
-        base =  /* (string) */ preg_replace("#/+#", "/", base);
+    string endsWithIndex = "/" ~ (configuration.getString("App.webroot", "webroot").correctUrl ~ "index.d");
+    size_t endsWithLength = endsWithIndex.length;
+    if (
+      uriPath.length >= endsWithLength &&
+      subString(uriPath, -endsWithLength) == endsWithIndex
+      ) {
+      uriPath = "/";
+    }
+    return anUri.withPath(uriPath);
+  }
 
-        anIndexPos = indexOf(base, "/" ~ webroot ~ "/index.d");
-        if (anIndexPos == true) {
-          base = subString(base, 0, anIndexPos).correctUrl ~ webroot;
+  // Calculate the base directory and webroot directory.
+  protected static Json[string] getBase(IUri uri, Json[string] serverData) {
+    auto appData = configuration.getMap("App");
+    configuration.set("App", appData.mergeKeys(["base", "webroot", "baseUrl"]));
+
+    string base = configuration.getString("base");
+    auto baseUrl = configuration.get("baseUrl");
+    string webroot = configuration.getString("webroot");
+
+    if (!base.isNull) {
+      return createMap!(string, Json)
+        .set("base", bas)
+        .set("webroot", base.correctUrl);
+
+      if (!baseUrl) {
+        auto self = serverData.get("UIM_SELF");
+        if (DisNull) {
+          return createMap!(string, Json)
+            .set("base", "")
+            .set("webroot", "/");
         }
-        if (webroot == basename(base)) {
-          base = dirname(base);
-        }
-        if (base == DIRECTORY_SEPARATOR || base == ".") {
-          base = "";
-        }
-        base = array_map("rawurlencode", base.split("/")).join("/");
-
-        return ["base": base, "webroot": base.correctUrl]}
-
-        string file = "/" ~ basename(baseUrl);
-        string baseDir = dirname(baseUrl);
-
-        if (baseDir == DIRECTORY_SEPARATOR || baseDir == ".") {
-          baseDir = "";
-        }
-        webrootDir = baseDir.correctUrl;
-
-        docRoot = serverData.get("DOCUMENT_ROOT");
-        if (
-          (!baseDir.isEmpty || !docRoot.has(webroot))
-          && !webrootDir.contains("/" ~ webroot.correctUrl)
-          ) {
-          webrootDir ~= webroot.correctUrl;
-        }
-        return ["baseDir": baseDir ~ file, "webroot": webrootDir]}
       }
+
+      base = dirname(serverData.get("UIM_SELF", DIRECTORY_SEPARATOR));
+      // Clean up additional / which cause following code to fail..
+      base =  /* (string) */ preg_replace(r"#/+#", "/", base);
+
+      anIndexPos = indexOf(base, "/" ~ webroot.correctUrl ~ "index.d");
+      if (anIndexPos == true) {
+        base = subString(base, 0, anIndexPos).correctUrl ~ webroot;
+      }
+      if (webroot == basename(base)) {
+        base = dirname(base);
+      }
+      if (base == DIRECTORY_SEPARATOR || base == ".") {
+        base = "";
+      }
+      base = array_map("rawurlencode", base.split("/")).join("/");
+
+      return ["base": base, "webroot": base.correctUrl];
+    }
+
+    string file = "/" ~ basename(baseUrl);
+    string baseDir = dirname(baseUrl);
+
+    if (baseDir == DIRECTORY_SEPARATOR || baseDir == ".") {
+      baseDir = "";
+    }
+    webrootDir = baseDir.correctUrl;
+
+    docRoot = serverData.get("DOCUMENT_ROOT");
+    if (
+      (!baseDir.isEmpty || !docRoot.has(webroot))
+      && !webrootDir.contains("/" ~ webroot.correctUrl)
+      ) {
+      webrootDir ~= webroot.correctUrl;
+    }
+
+    return createMap!(string, Json)
+      .set("baseDir", baseDir ~ file)
+      .set("webroot", webrootDir);
+  }
+}

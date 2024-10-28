8c4033f1800c62e090a8c958049bba8eefe01e76
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-28 16:24:33 +0100

    Rebuilding uim-databases

---
+++ databases/uim/databases/package.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.databases;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // uim.filesystem libraries
+  import uim.databases.classes;
+  import uim.databases.enumerations;
+  import uim.databases.errors;
+  import uim.databases.exceptions;
+  import uim.databases.helpers;
+  import uim.databases.interfaces;
+  import uim.databases.mixins;
+  import uim.databases.tests;
+}

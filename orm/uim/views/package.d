121e6cea980c39ed3d5b363206704a8f7ea03275
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-28 16:36:00 +0100

    Rebuilding uim-orm

---
+++ orm/uim/views/package.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.orm;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // uim.filesystem libraries
+  import uim.orm.classes;
+  import uim.orm.enumerations;
+  import uim.orm.errors;
+  import uim.orm.exceptions;
+  import uim.orm.helpers;
+  import uim.orm.interfaces;
+  import uim.orm.mixins;
+  import uim.orm.tests;
+}

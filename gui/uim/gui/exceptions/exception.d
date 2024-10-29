251493a0adf4218115bca79240c84f43b7cbdaad
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 07:08:17 +0100

    Working on gui

---
+++ gui/uim/gui/exceptions/exception.d
@@ -0,0 +1,6 @@
+/****************************************************************************************************************
+* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.gui.exceptions.exception;import uim.gui;@safe:// I18n exception.class DGuiException : UIMException {  mixin(ExceptionThis!("Gui"));  override bool initialize(Json[string] initData = null) {    if (!super.initialize(initData)) {      return false;    }    messageTemplate("default", "Exception in libary uim-gui");    return true;  }}mixin(ExceptionCalls!("Gui"));unittest {  testException(GuiException);}

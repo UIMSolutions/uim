cb65124e5ccf9a1e0b76e8a3ad708e8032ce0136
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 07:00:23 +0100

    Working on sites

---
+++ sites/uim/sites/package.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.sites;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // uim.filesystem libraries
+  import uim.sites.classes;
+  import uim.sites.enumerations;
+  import uim.sites.errors;
+  import uim.sites.exceptions;
+  import uim.sites.helpers;
+  import uim.sites.interfaces;
+  import uim.sites.mixins;
+  import uim.sites.tests;
+}

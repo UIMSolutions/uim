e7964bd0e198598fbeee7c9310682d5ed0d27dba
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 12:40:33 +0100

    Working on uim-apps

---
+++ apps/uim/views/package.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.apps;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // uim.filesystem libraries
+  import uim.apps.classes;
+  import uim.apps.enumerations;
+  import uim.apps.errors;
+  import uim.apps.exceptions;
+  import uim.apps.helpers;
+  import uim.apps.interfaces;
+  import uim.apps.mixins;
+  import uim.apps.tests;
+}

d3c42368882777c1fa7f7bcad0d7c5d8ac4a348a
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 07:08:25 +0100

    Working on html

---
+++ html/uim/html/snippets/package.d
@@ -0,0 +1,6 @@
+/****************************************************************************************************************
+* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.html.snippets;public {    import uim.html.snippets.client;    import uim.html.snippets.dynamic;    import uim.html.snippets.multiitem;    import uim.html.snippets.product;}

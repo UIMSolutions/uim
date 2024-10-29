5bc31c364d451ef2e87dab506a7f7c17fed7a48f
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 13:00:04 +0100

    Working on uim-css

---
+++ css/uim/css/exceptions/exception.d
@@ -0,0 +1,6 @@
+/****************************************************************************************************************
+* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.css.exceptions.exception;import uim.css;@safe:// Base css exception.class DCssException : UIMException {  mixin(ExceptionThis!("Css"));  override bool initialize(Json[string] initData = null) {    if (!super.initialize(initData)) {      return false;    }    messageTemplate("default", "Exception in libary uim-css");    return true;  }}mixin(ExceptionCalls!("Css"));unittest {  assert(CssException);}

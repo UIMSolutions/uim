7bc0314b877892d73a5df171fc3c89600d038bbf
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 07:09:28 +0100

    Working on i18n

---
+++ i18n/uim/i18n/package.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.i18n;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // uim.filesystem libraries
+  import uim.i18n.classes;
+  import uim.i18n.enumerations;
+  import uim.i18n.errors;
+  import uim.i18n.exceptions;
+  import uim.i18n.helpers;
+  import uim.i18n.interfaces;
+  import uim.i18n.mixins;
+  import uim.i18n.tests;
+}

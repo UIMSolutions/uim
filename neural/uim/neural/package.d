272fac245cd48b21bb929a8b573bbed761704ec6
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 13:08:09 +0100

    Working on uim-neural

---
+++ neural/uim/neural/package.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.neural;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // uim.filesystem libraries
+  import uim.neural.classes;
+  import uim.neural.enumerations;
+  import uim.neural.errors;
+  import uim.neural.exceptions;
+  import uim.neural.helpers;
+  import uim.neural.interfaces;
+  import uim.neural.mixins;
+  import uim.neural.tests;
+}

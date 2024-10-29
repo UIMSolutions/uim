9ae627ff908fa817532bdd95fc13d7e9caf0028f
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 13:00:14 +0100

    Working on uim-errors

---
+++ errors/uim/errors/package.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.errors;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // uim.filesystem libraries
+  import uim.errors.classes;
+  import uim.errors.enumerations;
+  import uim.errors.errors;
+  import uim.errors.exceptions;
+  import uim.errors.helpers;
+  import uim.errors.interfaces;
+  import uim.errors.mixins;
+  import uim.errors.tests;
+}

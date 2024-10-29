f7403c34dc4f430aac1aee21496367226f3f69a8
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-29 13:02:30 +0100

    Working on uim-http

---
+++ http/uim/http/package.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.http;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // uim.filesystem libraries
+  import uim.http.classes;
+  import uim.http.enumerations;
+  import uim.http.errors;
+  import uim.http.exceptions;
+  import uim.http.helpers;
+  import uim.http.interfaces;
+  import uim.http.mixins;
+  import uim.http.tests;
+}

18610f5158c6d25b870fc9c98a41a9c45ef8740f
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-31 15:50:38 +0100

    Little Bugfixing

--- orm/uim/orm/mixins/association.d
+++ orm/uim/orm/mixins/association.d
@@ -1,5 +1,3 @@
-module uim.views.mixins.association;
-
 /****************************************************************************************************************
 * Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *

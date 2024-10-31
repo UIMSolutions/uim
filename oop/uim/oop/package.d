a9ca53a749cbdadeca0ba101a7fa6a41869ff607
UIMSolutions <ui.manufaktur@gmail.com>
2024-10-31 15:48:32 +0100

    Working on uim-oop

--- oop/uim/oop/package.d
+++ oop/uim/oop/package.d
@@ -18,22 +18,11 @@ public import vibe.d;
 // UIM libraries
 public import uim.core;
 
-// uim-oop packages/modules
-// public import uim.oop.annotations;
-// public import uim.oop.mixins;
-
-// module
-// public import uim.oop.obj;
-
-// packages
-// public import uim.oop.collections;	
 public {
-  import uim.oop.complex;
   import uim.oop.commands;
   import uim.oop.configurations;
   import uim.oop.containers;
   import uim.oop.containers.maps;
-  import uim.oop.core;
   import uim.oop.direntries;
   import uim.oop.enumerations;
   import uim.oop.errors;
@@ -41,15 +30,9 @@ public {
   import uim.oop.helpers;
   import uim.oop.formatters;
   import uim.oop.interfaces;
-  import uim.oop.keypairs;
   import uim.oop.mixins;
   import uim.oop.patterns;
-  import uim.oop.observers;
   import uim.oop.parsers;
-
-  // TODO import uim.oop.plugins;
-  import uim.oop.properties;
-  import uim.oop.registries;
   import uim.oop.simple;
   import uim.oop.tests;
   import uim.oop.tools;

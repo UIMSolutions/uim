d98792ce4be9564dd2a64109f3c9b8f6da033414
UIMSolutions <ui.manufaktur@gmail.com>
2024-11-08 11:35:46 +0100

    Code cleaning

--- entitybases/uim/entitybases/exceptions/exception.d
+++ entitybases/uim/entitybases/exceptions/exception.d
@@ -3,4 +3,29 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.entitybases.exceptions.exception;import uim.entitybases;@safe:// Datasource exception.class DEntitybasesException : UIMException {  mixin(ExceptionThis!("Entitybases"));  override bool initialize(Json[string] initData = null) {    if (!super.initialize(initData)) {      return false;    }    messageTemplate("default", "Exception in libary uim-entitybases");    return true;  }}mixin(ExceptionCalls!("Entitybases"));unittest {  testException(EntitybasesException);}
+module uim.entitybases.exceptions.exception;
+
+import uim.entitybases;
+
+@safe:
+
+// Datasource exception.
+class DEntitybasesException : UIMException {
+  mixin(ExceptionThis!("Entitybases"));
+
+  override bool initialize(Json[string] initData = null) {
+    if (!super.initialize(initData)) {
+      return false;
+    }
+
+    messageTemplate("default", "Exception in libary uim-entitybases");
+
+    return true;
+  }
+}
+
+mixin(ExceptionCalls!("Entitybases"));
+
+unittest {
+  testException(EntitybasesException);
+}

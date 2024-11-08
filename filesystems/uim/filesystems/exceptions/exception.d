08340fb1097eec9a1722dd80ef9d07ec479b7038
UIMSolutions <ui.manufaktur@gmail.com>
2024-11-08 11:28:10 +0100

    Code cleaning

--- filesystems/uim/filesystems/exceptions/exception.d
+++ filesystems/uim/filesystems/exceptions/exception.d
@@ -3,4 +3,29 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.filesystems.exceptions.exception;import uim.filesystems;@safe:// I18n exception.class DFilesystemsException : UIMException {  mixin(ExceptionThis!("Filesystems"));  override bool initialize(Json[string] initData = null) {    if (!super.initialize(initData)) {      return false;    }    messageTemplate("default", "Exception in libary uim-filesystems");    return true;  }}mixin(ExceptionCalls!("Filesystems"));unittest {  assert(FilesystemsException);}
+module uim.filesystems.exceptions.exception;
+
+import uim.filesystems;
+
+@safe:
+
+// I18n exception.
+class DFilesystemsException : UIMException {
+  mixin(ExceptionThis!("Filesystems"));
+
+  override bool initialize(Json[string] initData = null) {
+    if (!super.initialize(initData)) {
+      return false;
+    }
+
+    messageTemplate("default", "Exception in libary uim-filesystems");
+
+    return true;
+  }
+}
+
+mixin(ExceptionCalls!("Filesystems"));
+
+unittest {
+  assert(FilesystemsException);
+}

b545145f223573371686263804a812ce3f7da656
UIMSolutions <ui.manufaktur@gmail.com>
2024-12-31 09:25:30 +0100

    Add exceptions for routing errors in uim.routings.exceptions package

---
+++ routings/uim/routings/exceptions/missingdispatcherfilter.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.routings.exceptions.missingdispatcherfilter;
+
+import uim.routings;
+
+@safe:
+
+// Exception raised when a Dispatcher filter could not be found
+class DMissingDispatcherFilterException : UIMException {
+    mixin(ExceptionThis!("MissingDispatcherFilter"));
+
+    override bool initialize(Json[string] initData = null) {
+        if (!super.initialize(initData)) {
+            return false;
+        }
+
+        messageTemplate("default", "Dispatcher filter `%s` could not be found.");
+
+        return true;
+    }
+}
+
+mixin(ExceptionCalls!("MissingDispatcherFilter"));
+
+unittest {
+  assert(testException(MissingDispatcherFilterException));
+}

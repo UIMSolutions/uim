cf49237e13386e811762ffa3baecf57a9030fc16
UIMSolutions <ui.manufaktur@gmail.com>
2024-12-31 16:43:21 +0100

    Add time class and update package structure for internationalization

--- i18n/uim/i18n/classes/package.d
+++ i18n/uim/i18n/classes/package.d
@@ -1,8 +1,14 @@
 module uim.i18n.classes;
 
-public {
+public { // modules
+    import uim.i18n.classes.date;
+    import uim.i18n.classes.datetime;
     import uim.i18n.classes.i18n;
+    import uim.i18n.classes.pluralrules;
+    import uim.i18n.classes.time;
+}
 
+public { // packages
     import uim.i18n.classes.catalogs;
     import uim.i18n.classes.formatters;
     import uim.i18n.classes.loaders;

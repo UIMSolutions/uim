0fbe81282f7d7c9523f66729ab7bd03861b7a40d
UIMSolutions <ui.manufaktur@gmail.com>
2024-12-31 15:57:41 +0100

    Update version numbers in dub.selections.json and add date formatting mixins for internationalization

--- i18n/uim/i18n/helpers/package.d
+++ i18n/uim/i18n/helpers/package.d
@@ -1,2 +1,5 @@
 module uim.i18n.helpers;
 
+public {
+    import uim.i18n.helpers.functions;
+}
--- i18n/uim/i18n/mixins/package.d
+++ i18n/uim/i18n/mixins/package.d
@@ -2,6 +2,7 @@ module uim.i18n.mixins;
 
 public {
     import uim.i18n.mixins.catalog;
+    import uim.i18n.mixins.dateformat;
     import uim.i18n.mixins.formatter;
     import uim.i18n.mixins.loader;
     import uim.i18n.mixins.middleware;

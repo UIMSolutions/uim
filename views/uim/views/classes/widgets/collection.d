06f25523d7a5bcb0b25f87097da97680bbe492a7
UIMSolutions <ui.manufaktur@gmail.com>
2025-01-11 09:32:07 +0100

    Add widget classes and registry for uim.views.classes.widgets

---
+++ views/uim/views/classes/widgets/collection.d
@@ -0,0 +1,24 @@
+/****************************************************************************************************************
+* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.views.classes.widgets.collection;
+
+import uim.views;
+
+@safe:
+
+// An object Collection for Widget.
+class DWidgetCollection : DCollection!DWidget {
+}
+
+auto WidgetCollection() {
+  return new DWidgetCollection;
+}
+
+unittest {
+  assert(WidgetCollection);
+
+  auto widgets = WidgetCollection;
+}

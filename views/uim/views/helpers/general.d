9ba2dc5c4b4f1e2e2a50642c107fb9d28761a37c
UIMSolutions <ui.manufaktur@gmail.com>
2025-03-02 07:46:20 +0100

    Daily Works

--- views/uim/views/helpers/general.d
+++ views/uim/views/helpers/general.d
@@ -1,10 +1,6 @@
 /****************************************************************************************************************
-* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* Copyright: © 2017-2024 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.logging.enumerations;
-
-public {
-    import uim.logging.enumerations.formats;
-}
+module uim.views.helpers.general;import uim.views;@safe: unittest {  writeln("-----  ", __MODULE__ , "\t  -----");}

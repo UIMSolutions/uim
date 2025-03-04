1d70604938909aac046ced06543749f4c031f1e2
UIMSolutions <ui.manufaktur@gmail.com>
2025-03-04 06:48:11 +0100

    Remove obsolete README and package files from uim.core modules

--- core/uim/core/convert/package.d
+++
@@ -1,9 +0,0 @@
-module uim.core.convert;
-
-public {
-  import uim.core.convert.json;
-  import uim.core.convert.jsonmap;
-  import uim.core.convert.string_;
-  import uim.core.convert.stringmap;
-  import uim.core.convert.uuid;
-}

72ad2e3ab270b7fe457185f6bcf0c0ee90e9792f
UIMSolutions <ui.manufaktur@gmail.com>
2025-03-04 07:30:11 +0100

    Remove obsolete uim.oop packages and related files

--- oop/uim/oop/helpers/exceptions/xml.d
+++
@@ -1,9 +0,0 @@
-module uim.oop.helpers.exceptions.xml;
-
-import uim.oop;
-
-@safe:
-
-// Exception class for Xml. This exception will be thrown from Xml when it encounters an error.
-/* class XmlException : DException {
-} */

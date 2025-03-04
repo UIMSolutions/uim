b93e0dd0f307f7c0564e0824ac4725a25ebfb1d5
UIMSolutions <ui.manufaktur@gmail.com>
2025-03-04 07:46:38 +0100

    Add initial implementation of uim.databases library with modules, helpers, exceptions, mixins, and tests

---
+++ infra-level/databases/uim/databases/classes/drivers/sqlite.d
@@ -0,0 +1,95 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.databases.classes.drivers.sqlite;
+
+import uim.databases;
+
+@safe:
+
+version (test_uim_databases) {
+  unittest {
+    writeln("-----  ", __MODULE__, "\t  -----");
+  }
+}
+
+class DSqliteDriver : DDBDriver {
+  mixin(DBDriverThis!("Sqlite"));
+
+  override bool initialize(Json[string] initData = null) {
+    if (!super.initialize(initData)) {
+      return false;
+    }
+
+    // `mask` The mask used for created database
+    configuration
+      .merge("persistent", false.toJson)
+      .merge(["username", "password"], "")
+      .merge("database", ": memory:")
+      .merge("encoding", "utf8")
+      .merge("mask", Json( /*0*/ 644))
+      .merge(["cache", "mode"], Json(null))
+      .merge(["flags", "init"], Json.emptyArray);
+
+    startQuote("\"");
+    endQuote("\"");
+
+    return true;
+  }
+
+  // #region SQL
+  // Get the SQL for disabling foreign keys.
+  override string sqlDisableForeignKey() {
+    return "PRAGMA foreign_keys = OFF";
+  }
+
+  override string sqlEnableForeignKey() {
+    return "PRAGMA foreign_keys = ON";
+  }
+  // #endregion SQL
+
+  bool supports(DriverFeatures feature) {
+    switch (feature) {
+    case DriverFeatures.DISABLE_CONSTRAINT_WITHOUT_TRANSACTION,
+      DriverFeatures.SAVEPOINT,
+      DriverFeatures.TRUNCATE_WITH_CONSTRAINTS:
+      return true;
+
+    case DriverFeatures.JSON:
+      return false;
+
+    case  // TODO
+      DriverFeatures.CTE,
+      DriverFeatures.WINDOW:
+      return false;
+      /* return version_compare(
+                this.currentVersion(),
+                this.featureVersions.getString("feature.value"),
+                ">="
+           ); */
+    default:
+      return false;
+    };
+  }
+
+  override IDBDriver connect() {
+    super.connect();
+    // TODO
+    return this;
+  }
+
+  override IDBDriver disconnect() {
+    super.disconnect();
+    // TODO
+    return this;
+  }
+}
+
+mixin(DBDriverCalls!("Sqlite"));
+
+unittest {
+  auto driver = SqliteDriver;
+  assert(testDBDriver(driver));
+}
